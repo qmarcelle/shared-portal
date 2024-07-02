413a7e59125bb00c211e9d57c076e5038852fd52
Emily Elam <Emily_Elam@bcbst.com>
2024-07-02 15:58:45 -0400

    NextAuth middleware and routing config

---
+++ src/utils/routes.ts
@@ -0,0 +1,21 @@
+/**
+ * Routes that are accessible to non-logged-in clients.
+ */
+export const publicRoutes = [
+    "/"
+];
+
+/**
+ * Routes that are used for authentication.
+ * The routes will redirect logged-in users to /settings.
+ */
+export const authRoutes = [
+    "/login"
+];
+
+/**
+ * Context root for API authentication routes.
+ */
+export const apiAuthPrefix = "/api";
+
+export const DEFAULT_LOGIN_REDIRECT = process.env.WPS_REDIRECT_URL || "";
