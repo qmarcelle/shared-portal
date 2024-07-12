a2ce330a5675db9b93a1a5803efcf43885e08fbf
Rohit Namala <Rohit_Namala@bcbst.com>
2024-07-12 18:55:52 +0530

    Primary Care Options Updates (#22)

---
+++ src/tests/app/primaryCareOptions/FindInNetworkProvider.spec.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { InfoCard } from '../../../components/composite/InfoCard';
+import FindCare from '../../../public/assets/Find-Care.svg';
+
+const renderUI = () => {
+  return render(
+    <InfoCard
+      icon={FindCare}
+      label="Find an In-network Provider"
+      body="Find a high-quality provider for either in-person or virtual telehealth appointments."
+    />,
+  );
+};
+
+describe('FindInNetworkProviderSection', () => {
+  it('should render the UI correctly', async () => {
+    const component = renderUI();
+    screen.getByText('Find an In-network Provider');
+    screen.getByText(
+      'Find a high-quality provider for either in-person or virtual telehealth appointments.',
+    );
+    screen.getByAltText(/link/i);
+    expect(component).toMatchSnapshot();
+  });
+});
