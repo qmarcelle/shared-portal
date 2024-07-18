e55f07a47763385fbee9aa8d4b41253abbb80ef7
Emily Elam <Emily_Elam@bcbst.com>
2024-07-18 15:46:05 -0400

    Restructure authorize() to get around TS strict issue

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,12 @@
-import type { NextAuthConfig } from 'next-auth';
+import { CredentialsSignin, type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import { PortalUser } from './models/auth/user';
 import { getPersonBusinessEntity } from './utils/api/client/get_pbe';
+
+class AuthError extends CredentialsSignin {
+  code =
+    "We're sorry, we weren't able to authenticate your account due to an unknown error. Please try again later.";
+}
 
 /**
  * This NextAuth provider does NOT process authentication!
@@ -13,14 +19,22 @@ export default {
       credentials: {
         userId: { label: 'User ID' },
       },
-      async authorize(credentials) {
-        try {
-          const user = await getPersonBusinessEntity(
-            credentials.userId.toString(),
+      async authorize(credentials): Promise<PortalUser> {
+        const username = credentials.userId?.toString();
+        if (!username) {
+          console.error(
+            `Tried to create session with an empty username. Something is very wrong!`,
           );
+          throw new AuthError();
+        }
+        try {
+          const user = await getPersonBusinessEntity(username);
           return user;
-        } catch (error) {}
-        return null;
+        } catch (error) {
+          console.error(`Failed to create user session!`);
+          console.error(error);
+          throw new AuthError();
+        }
       },
     }),
   ],
