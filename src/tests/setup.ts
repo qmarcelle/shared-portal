fb5e7d179ef08dec4c76aaac0795cbc3903c3333
Emily Elam <Emily_Elam@bcbst.com>
2024-07-18 14:29:41 -0400

    Add missing properties to fix Jest TS error

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -3,7 +3,14 @@ window.matchMedia =
   function () {
     return {
       matches: false,
+      media: null,
+      onchange: null,
       addListener: function () {},
       removeListener: function () {},
+      addEventListener: function () {},
+      removeEventListener: function () {},
+      dispatchEvent: function () {
+        return false;
+      },
     };
   };
