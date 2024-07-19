d54896b1e78a9b618a83bd6b6f2dbbfd7814d1d8
Rohit Namala <Rohit_Namala@bcbst.com>
2024-07-19 18:27:53 +0530

    Site Header Updates from old repo (#31)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @next/next/no-sync-scripts */
 'use client';
 
 import '@/app/globals.css';
 import { AppModal } from '@/components/foundation/AppModal';
+import { SideBarModal } from '@/components/foundation/SideBarModal';
 import '@/styles/base.css';
 import '@/styles/checkbox.css';
 import 'react-responsive-modal/styles.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+import SiteHeader from '../components/foundation/SiteHeader';
 import ClientLayout from './ClientLayout';
 
 export default function RootLayout({
@@ -17,8 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <SideBarModal />
+        <SiteHeader />
         <ClientLayout>{children}</ClientLayout>
         <AppModal />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
       </body>
     </html>
   );
---
+++ src/components/foundation/SiteHeader.tsx
@@ -0,0 +1,127 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { SiteHeaderNavSection } from '../composite/SiteHeaderNavSection';
+import { SiteHeaderSubNavSection } from '../composite/SiteHeaderSubNavSection';
+import pages from '../pages';
+import { SiteHeaderMenuSection } from './../composite/SiteHeaderMenuSection';
+import {
+  bcbstBlueLogo,
+  bcbstStackedlogo,
+  closeIcon,
+  hamburgerMenuIcon,
+  idCardIcon,
+  inboxIcon,
+  profileWhiteIcon,
+} from './Icons';
+
+export default function SiteHeader() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="primary-color">
+      {/* Header Top Bar */}
+      <div className="absolute h-18 w-full lg:static flex justify-between border-b bg-white z-30">
+        <div className="flex items-center">
+          <div className="flex lg:hidden h-18 w-18 items-center justify-center border-r">
+            <button
+              data-collapse-toggle="menu-bar"
+              type="button"
+              className="p-0 justify-center"
+              aria-controls="menu-bar"
+              aria-expanded="false"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              {isOpen ? (
+                <>
+                  <span className="sr-only">Close main menu</span>
+                  <Image
+                    src={closeIcon}
+                    alt={'Close icon'}
+                    width="18"
+                    height="18"
+                  ></Image>
+                </>
+              ) : (
+                <>
+                  <span className="sr-only">Open main menu</span>
+                  <Image
+                    src={hamburgerMenuIcon}
+                    alt={'Menu icon'}
+                    width="32"
+                    height="32"
+                  ></Image>
+                </>
+              )}
+            </button>
+          </div>
+          <Link className="ml-5 lg:px-0" href="/dashboard">
+            {useMediaQuery({ query: '(max-width: 1023px)' }) ? (
+              <Image
+                width="64"
+                height="36"
+                src={bcbstStackedlogo}
+                alt="BCBST Stacked Logo"
+              />
+            ) : (
+              <Image
+                width="174"
+                height="35"
+                src={bcbstBlueLogo}
+                alt="BCBST Logo"
+              />
+            )}
+          </Link>
+        </div>
+        <SiteHeaderMenuSection
+          user={'Chris Hall'}
+          icon={<Image src={profileWhiteIcon} alt="Profile Icon"></Image>}
+          items={[
+            {
+              title: 'Inbox',
+              label: 'inbox',
+              icon: <Image src={inboxIcon} alt="Inbox" />,
+              url: 'inbox',
+            },
+            {
+              title: 'ID Card',
+              label: 'id card',
+              icon: <Image src={idCardIcon} alt="ID Card" />,
+              url: 'idcard',
+            },
+          ]}
+        />
+      </div>
+      {/* Header Nav Bar */}
+      <div
+        id="menu-bar"
+        className="hidden absolute top-[72px] lg:static lg:block w-full md:w-1/2 lg:w-full bg-white z-40"
+        data-accordion="collapse"
+      >
+        <div className="flex font-bold">
+          <SiteHeaderNavSection parentPages={pages} />
+        </div>
+        <div className="absolute top-0 lg:static w-full lg:w-full bg-white z-50 border-r lg:border-0">
+          {pages.map((page, index) => (
+            <SiteHeaderSubNavSection
+              key={index}
+              id={page.id}
+              title={page.title}
+              description={page.description}
+              category={page.category}
+              showOnMenu={page.showOnMenu}
+              url={page.url}
+              qt={page.qt}
+              childPages={page.childPages}
+              template={page.template}
+            />
+          ))}
+        </div>
+      </div>
+      {/* NavSub Bars */}
+    </nav>
+  );
+}
