80a8de8f7b0c23af751449fa038f623e6163e92a
Qwynn Marcelle <qwynn_marcelle@bcbst.com>
2024-07-22 17:03:58 -0400

    prettier quote fix

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	semi: true,
-	singleQuote: true,
-	trailingComma: "all",
-	printWidth: 80,
-	tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
 };
