bf98ac08ab71ef6feaae7d54e10ad9c050ae3079
Rohit Namala <Rohit_Namala@bcbst.com>
2024-07-29 17:36:33 +0530

    search results page updates and enhancements (#59)

--- src/app/searchResults/components/SearchBanner.tsx
+++ src/app/searchResults/components/SearchBanner.tsx
@@ -4,21 +4,25 @@ import { Column } from '@/components/foundation/Column';
 import { Header } from '@/components/foundation/Header';
 import { Spacer } from '@/components/foundation/Spacer';
 import { TextBox } from '@/components/foundation/TextBox';
-import externalIcon from '../../../../public/assets/External-White.svg';
 import Image from 'next/image';
+import externalIcon from '../../../../public/assets/External-White.svg';
 
 export const SearchBanner = () => {
   return (
     <Card className="searchBanner container">
       <Column>
         <Spacer size={16} />
-        <Header className="title-3" text="Talk With a Care Provider" />
+        <Header className="title-3 pl-2" text="Talk With a Care Provider" />
         <Spacer size={16} />
-        <TextBox text="Schedule an appointment or get health information 24/7 for non-emergency conditions." />
-        <Spacer size={32} />
+        <TextBox
+          className="pl-2"
+          text="Schedule an appointment or get health information 24/7 for non-emergency conditions."
+        />
+        <Spacer size={16} />
         <AppLink
           label="Get Started with Teladoc Health"
-          className="searchBannerLink"
+          className="searchBannerLink inline"
+          displayStyle="inline-flex"
           icon={<Image src={externalIcon} alt="external" />}
         />
       </Column>
