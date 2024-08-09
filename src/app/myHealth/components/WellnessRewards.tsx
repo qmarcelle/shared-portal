0b74382a24dab2444086ada7a2af67427a92db17
Rohit Namala <rohit_namala@bcbst.com>
2024-08-09 16:18:48 +0530

    Wellness rewards and member discounts components

--- src/app/myHealth/components/WellnessRewards.tsx
+++ src/app/myHealth/components/WellnessRewards.tsx
@@ -7,7 +7,6 @@ import { Spacer } from '@/components/foundation/Spacer';
 import { TextBox } from '@/components/foundation/TextBox';
 import Image from 'next/image';
 import { ReactNode } from 'react';
-import { useMediaQuery } from 'react-responsive';
 import { IComponent } from '../../../components/IComponent';
 import { Card } from '../../../components/foundation/Card';
 import { Column } from '../../../components/foundation/Column';
@@ -39,7 +38,6 @@ export const WellnessRewards = ({
   icon = <Image alt="external icon" src={externalOffsiteWhiteIcon} />,
   quarter,
 }: WellnessRewardsProps) => {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
   function getQuarterRange() {
     if (quarter == 'Q1') {
       return 'JAN - APR';
@@ -50,72 +48,13 @@ export const WellnessRewards = ({
     }
   }
 
-  function getDesktopView() {
-    return (
-      <Card className={className}>
-        <>
-          <Header type="title-2" text="Wellness Rewards" />
-          <Spacer size={25} />
-          <Row className="justify-between">
-            <Column className="w-1/2">
-              <TextBox text={`${quarter} | ${getQuarterRange()}`} />
-              <Spacer size={5} />
-              <TextBox text="My Points" />
-              <Spacer size={16} />
-              <ProgressBar
-                height={10}
-                completePercent={
-                  (quarterlyPointsEarned / quarterlyMaxPoints) * 100
-                }
-              />
-              <Spacer size={12} />
-              <Row className="justify-between">
-                <Column>
-                  <TextBox
-                    className="font-bold"
-                    text={`${quarterlyPointsEarned} pts`}
-                  />
-                  <TextBox text="Earned" />
-                </Column>
-                <Column>
-                  <TextBox
-                    className="font-bold text-end"
-                    text={`${quarterlyMaxPoints} pts`}
-                  />
-                  <TextBox text="Quarterly Max" />
-                </Column>
-              </Row>
-              <Spacer size={18} />
-              <Row className="items-center">
-                <TextBox text="100 points = $100" />
-                &nbsp;|
-                <AppLink label="View Rewards FAQ" />
-              </Row>
-              <Spacer size={18} />
-              <Button label={linkText} icon={icon} callback={() => null} />
-            </Column>
-            <Divider axis="horizontal" />
-            <Column>
-              <WellnessRewardsChart
-                color1={color1}
-                color2={color2}
-                earnedAmount={totalAmountEarned}
-                maxAmount={totalAmount}
-              />
-            </Column>
-          </Row>
-        </>
-      </Card>
-    );
-  }
-
-  function getMobileView() {
-    return (
-      <Card className={className}>
-        <>
-          <Header type="title-2" text="Wellness Rewards" />
-          <Spacer size={25} />
-          <Column>
+  return (
+    <Card className={className}>
+      <>
+        <Header type="title-2" text="Wellness Rewards" />
+        <Spacer size={25} />
+        <Row className="justify-between hidden sm:flex">
+          <Column className="w-1/2">
             <TextBox text={`${quarter} | ${getQuarterRange()}`} />
             <Spacer size={5} />
             <TextBox text="My Points" />
@@ -152,9 +91,7 @@ export const WellnessRewards = ({
             <Spacer size={18} />
             <Button label={linkText} icon={icon} callback={() => null} />
           </Column>
-          <Spacer size={32} />
-          <Divider />
-          <Spacer size={25} />
+          <Divider axis="horizontal" />
           <Column>
             <WellnessRewardsChart
               color1={color1}
@@ -163,10 +100,54 @@ export const WellnessRewards = ({
               maxAmount={totalAmount}
             />
           </Column>
-        </>
-      </Card>
-    );
-  }
+        </Row>
+        <Column className="block sm:hidden">
+          <TextBox text={`${quarter} | ${getQuarterRange()}`} />
+          <Spacer size={5} />
+          <TextBox text="My Points" />
+          <Spacer size={16} />
+          <ProgressBar
+            height={10}
+            completePercent={(quarterlyPointsEarned / quarterlyMaxPoints) * 100}
+          />
+          <Spacer size={12} />
+          <Row className="justify-between">
+            <Column>
+              <TextBox
+                className="font-bold"
+                text={`${quarterlyPointsEarned} pts`}
+              />
+              <TextBox text="Earned" />
+            </Column>
+            <Column>
+              <TextBox
+                className="font-bold text-end"
+                text={`${quarterlyMaxPoints} pts`}
+              />
+              <TextBox text="Quarterly Max" />
+            </Column>
+          </Row>
+          <Spacer size={18} />
+          <Row className="items-center">
+            <TextBox text="100 points = $100" />
+            &nbsp;|
+            <AppLink label="View Rewards FAQ" />
+          </Row>
+          <Spacer size={18} />
+          <Button label={linkText} icon={icon} callback={() => null} />
 
-  return isMobile ? getMobileView() : getDesktopView();
+          <Spacer size={32} />
+          <Divider />
+          <Spacer size={25} />
+
+          <WellnessRewardsChart
+            color1={color1}
+            color2={color2}
+            earnedAmount={totalAmountEarned}
+            maxAmount={totalAmount}
+          />
+        </Column>
+      </>
+    </Card>
+  );
 };
