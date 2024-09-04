bb6252e0db3781812c9e5e710bcd7b03d01bf799
Rohit Namala <rohit_namala@bcbst.com>
2024-09-04 22:42:57 +0530

    order id card modal

--- src/components/foundation/TextField.tsx
+++ src/components/foundation/TextField.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useState } from 'react';
+import { IComponent } from '../IComponent';
 import {
   alertErrorIcon,
   showPasswordIcon,
@@ -7,12 +8,12 @@ import {
 } from './Icons';
 import { Row } from './Row';
 
-export interface TextFieldProps {
+export interface TextFieldProps extends IComponent {
   label: string;
-  type?: 'text' | 'password' | 'email';
+  type?: 'text' | 'password' | 'email' | 'number';
   errors?: string[] | null;
   fillGuidance?: string[] | null;
-  value?: string;
+  value?: string | number;
   hint?: string;
   valueCallback?: (value: string) => void;
   onKeydownCallback?: (key: string) => void;
@@ -20,6 +21,8 @@ export interface TextFieldProps {
   maxWidth?: number;
   isSuffixNeeded?: boolean;
   onFocusCallback?: () => void;
+  minValue?: number;
+  maxValue?: number;
 }
 
 const ObscureIndicator = ({ obscure }: { obscure: boolean }) => {
@@ -47,7 +50,7 @@ const SuffixIcon = ({
   type,
   obscured,
 }: {
-  type: 'text' | 'password' | 'email';
+  type: 'text' | 'password' | 'email' | 'number';
   errors?: string[] | null;
   obscured?: boolean | null;
 }) => {
@@ -117,6 +120,9 @@ export const TextField = ({
   onKeydownCallback,
   maxWidth,
   isSuffixNeeded = false,
+  minValue,
+  maxValue,
+  className = '',
 }: TextFieldProps) => {
   const [focus, setFocus] = useState(false);
   const [obscuredState, setObscuredState] = useState(
@@ -145,7 +151,7 @@ export const TextField = ({
     >
       <p>{label}</p>
       <div
-        className={`flex flex-row items-center input ${
+        className={`flex flex-row items-center input ${className} ${
           focus ? 'input-focus' : ''
         } ${errors!.length > 0 ? 'error-input' : ''}`}
       >
@@ -161,6 +167,9 @@ export const TextField = ({
           value={value}
           placeholder={hint}
           type={computeType()}
+          min={minValue}
+          max={maxValue}
+          className={className}
         />
         <div className="cursor-pointer" onClick={toggleObscure}>
           {isSuffixNeeded && (
