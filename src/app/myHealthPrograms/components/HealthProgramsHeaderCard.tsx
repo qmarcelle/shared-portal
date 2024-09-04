c69f85f5108a2c48e09f8bd0a757fc775647fa91
n17299c <Navya_CS@bcbst.com>
2024-09-04 19:37:49 +0530

    Fixed mobile view icon review comment

--- src/app/myHealthPrograms/components/HealthProgramsHeaderCard.tsx
+++ src/app/myHealthPrograms/components/HealthProgramsHeaderCard.tsx
@@ -1,13 +1,14 @@
+import { Button } from '@/components/foundation/Button';
 import { Column } from '@/components/foundation/Column';
 import { Header } from '@/components/foundation/Header';
 import { externalOffsiteWhiteIcon } from '@/components/foundation/Icons';
 import { Row } from '@/components/foundation/Row';
 import { Spacer } from '@/components/foundation/Spacer';
 import { TextBox } from '@/components/foundation/TextBox';
-import { Button } from '@/components/foundation/Button';
-import Image from 'next/image';
 import { IComponent } from '@/components/IComponent';
+import Image from 'next/image';
 import { ReactNode } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { HealthProgramHeaderCardDetails } from '../models/health_program_header_card_details';
 
 interface HealthProgramHeaderProps extends IComponent {
@@ -19,39 +20,89 @@ export const HealthProgramsHeaderCard = ({
   icon = <Image alt="external icon" src={externalOffsiteWhiteIcon} />,
   healthProgramHeaderDetails,
 }: HealthProgramHeaderProps) => {
-  return (
-    <Row className="justify-between mt-16 lg:mt-0">
-      <Column className="pl-3 my-health-programs-header">
-        <Spacer size={8} />
-        <Header type="title-1" text={healthProgramHeaderDetails.title} />
-        <Spacer size={8} />
-        <TextBox
-          className="body-1"
-          text={healthProgramHeaderDetails.description}
-        />
-        <Spacer size={16} />
-        <TextBox
-          className="body-1"
-          text={healthProgramHeaderDetails.serviceDesc}
-        />
-        <Spacer size={23} />
-        <Button
-          icon={icon}
-          label={healthProgramHeaderDetails.buttonText}
-          className="my-health-programs-header-button"
-          callback={() => {
-            window.location.href =
-              healthProgramHeaderDetails.redirectLink ?? ' ';
-          }}
-        />
-      </Column>
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  function getDesktopView() {
+    return (
+      <Row className="justify-between mt-16 lg:mt-0">
+        <Column className="pl-3 my-health-programs-header">
+          <Spacer size={8} />
+          <Header type="title-1" text={healthProgramHeaderDetails.title} />
+          <Spacer size={8} />
+          <TextBox
+            className="body-1"
+            text={healthProgramHeaderDetails.description}
+          />
+          <Spacer size={16} />
+          <TextBox
+            className="body-1"
+            text={healthProgramHeaderDetails.serviceDesc}
+          />
+          <Spacer size={23} />
+          <Button
+            icon={icon}
+            label={healthProgramHeaderDetails.buttonText}
+            className="my-health-programs-header-button"
+            callback={() => {
+              window.location.href =
+                healthProgramHeaderDetails.redirectLink ?? ' ';
+            }}
+          />
+        </Column>
+        <Column>
+          <Image
+            src={healthProgramHeaderDetails.icon}
+            className="size-100"
+            alt="Info"
+          />
+        </Column>
+      </Row>
+    );
+  }
+
+  function getMobileView() {
+    return (
       <Column>
-        <Image
-          src={healthProgramHeaderDetails.icon}
-          className="size-100"
-          alt="Info"
-        />
+        <Column className="pl-2">
+          <Image
+            src={healthProgramHeaderDetails.icon}
+            className="size-100"
+            alt="Info"
+          />
+        </Column>
+        <Spacer size={8} />
+        <Column className="pl-3">
+          <Spacer size={8} />
+          <Header type="title-1" text={healthProgramHeaderDetails.title} />
+          <Spacer size={8} />
+          <TextBox
+            className="body-1"
+            text={healthProgramHeaderDetails.description}
+          />
+          <Spacer size={16} />
+          <TextBox
+            className="body-1"
+            text={healthProgramHeaderDetails.serviceDesc}
+          />
+          <Spacer size={23} />
+          <Button
+            icon={icon}
+            label={healthProgramHeaderDetails.buttonText}
+            className="my-health-programs-header-button"
+            callback={() => {
+              window.location.href =
+                healthProgramHeaderDetails.redirectLink ?? ' ';
+            }}
+          />
+        </Column>
       </Column>
-    </Row>
+    );
+  }
+
+  return (
+    <Column className="app-content app-base-font-color">
+      <Spacer size={64} />
+      {isMobile ? getMobileView() : getDesktopView()}
+    </Column>
   );
 };
