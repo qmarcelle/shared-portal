c657a647405d0b087020a11ec94497180b0277b9
Sundharam, Mythili Shanmuga <MythiliShanmuga_Sundharam@bcbst.com>
2024-09-24 12:07:21 +0000

    Merged PR 2843: FAQ_17711_UI_Changes
    Related work items: #17711

---
+++ src/app/support/faq/page.tsx
@@ -0,0 +1,94 @@
+'use client';
+import { InfoCard } from '@/components/composite/InfoCard';
+import { Column } from '@/components/foundation/Column';
+import { Header } from '@/components/foundation/Header';
+import {
+  benefits,
+  claimsBenefitsCoverage,
+  enrollment,
+  idCardPrimaryIconSvg,
+  keyIcon,
+  prescription,
+  priorAuthorizations,
+} from '@/components/foundation/Icons';
+import { Spacer } from '@/components/foundation/Spacer';
+import { TextBox } from '@/components/foundation/TextBox';
+
+const FrequentlyAskedQuestions = () => {
+  const faqInformationDetails = [
+    {
+      label: 'Benefits & Coverage',
+      description: 'Learn more about service limits, deductibles and more.',
+      iconName: benefits,
+      link: '/benefits',
+    },
+    {
+      label: 'Claims',
+      description: 'Learn more about claims or how to file a dispute.',
+      iconName: claimsBenefitsCoverage,
+      link: '/claims',
+    },
+    {
+      label: 'ID Cards',
+      description:
+        // eslint-disable-next-line quotes
+        "Do you have questions about your ID cards? We're here to help.",
+      iconName: idCardPrimaryIconSvg,
+      link: '/memberIDCard',
+    },
+    {
+      label: 'My Plan Information',
+      description: 'How to update your address, dependents, and more.',
+      iconName: enrollment,
+      link: '/spendingAccount',
+    },
+    {
+      label: 'Pharmacy',
+      description:
+        'How to find pharmacies, find prescription drug coverage and more.',
+      iconName: prescription,
+      link: '/spendingSummary',
+    },
+    {
+      label: 'Prior Authorization',
+      description:
+        'Learn more about prior authorizations, statuses and how to make appeals.',
+      iconName: priorAuthorizations,
+      link: '/priorAuth',
+    },
+    {
+      label: 'Sharing, Permissions & Security',
+      description:
+        'How to share your health insurance information, represent a dependent individual and more.',
+      iconName: keyIcon,
+      link: '/planDocuments',
+    },
+  ];
+
+  return (
+    <main className="flex flex-col justify-center items-center page">
+      <Column className="flex flex-col app-content">
+        <Spacer size={32} />
+        <section className="pl-5">
+          <Header className="title-1" text="Frequently Asked Questions" />
+          <Spacer size={16} />
+          <TextBox text="We’ve answered some of the most common questions about health insurance." />
+          <Spacer size={32} />
+        </section>
+        {faqInformationDetails.map((item) => {
+          return (
+            <InfoCard
+              key={item.label}
+              label={item.label}
+              icon={item.iconName}
+              body={item.description}
+              link={item.link}
+            />
+          );
+        })}
+      </Column>
+    </main>
+  );
+};
+
+export default FrequentlyAskedQuestions;
---
+++ src/tests/app/support/faqPage.spec.tsx
@@ -0,0 +1,58 @@
+import FrequentlyAskedQuestions from '@/app/support/faq/page';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+describe('Frequently Asked Question Page', () => {
+  it('should render the page correctly', () => {
+    const { container } = render(<FrequentlyAskedQuestions />);
+
+    screen.getByRole('heading', { name: 'Frequently Asked Questions' });
+    expect(
+      screen.getByText(
+        'We’ve answered some of the most common questions about health insurance.',
+      ),
+    ).toBeVisible();
+
+    expect(screen.getByText('Benefits & Coverage')).toBeVisible();
+    expect(
+      screen.getByText(
+        'Learn more about service limits, deductibles and more.',
+      ),
+    ).toBeVisible();
+    expect(screen.getByText('Claims')).toBeVisible();
+    expect(
+      screen.getByText('Learn more about claims or how to file a dispute.'),
+    ).toBeVisible();
+    expect(screen.getByText('ID Cards')).toBeVisible();
+    expect(
+      screen.getByText(
+        // eslint-disable-next-line quotes
+        "Do you have questions about your ID cards? We're here to help.",
+      ),
+    ).toBeVisible();
+    expect(screen.getByText('My Plan Information')).toBeVisible();
+    expect(
+      screen.getByText('How to update your address, dependents, and more.'),
+    ).toBeVisible();
+    expect(screen.getByText('Pharmacy')).toBeVisible();
+    expect(
+      screen.getByText(
+        'How to find pharmacies, find prescription drug coverage and more.',
+      ),
+    ).toBeVisible();
+    expect(screen.getByText('Prior Authorization')).toBeVisible();
+    expect(
+      screen.getByText(
+        'Learn more about prior authorizations, statuses and how to make appeals.',
+      ),
+    ).toBeVisible();
+    expect(screen.getByText('Sharing, Permissions & Security')).toBeVisible();
+    expect(
+      screen.getByText(
+        'How to share your health insurance information, represent a dependent individual and more.',
+      ),
+    ).toBeVisible();
+
+    expect(container).toMatchSnapshot();
+  });
+});
