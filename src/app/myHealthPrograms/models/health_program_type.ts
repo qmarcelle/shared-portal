6252be79d9219de7b8dac8b9900708ad1fda6c58
Rohit Namala <Rohit_Namala@bcbst.com>
2024-09-26 12:42:17 +0000

    Merged PR 2807: 16294 Diabetes Prevention page updates
    [US 16294 Testing Doc.docx](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/2807/attachments/US%2016294%20Testing%20Doc.docx) 16294 Diabetes Prevention page updates
    
    Related work items: #16294

--- src/app/myHealthPrograms/models/health_program_type.ts
+++ src/app/myHealthPrograms/models/health_program_type.ts
@@ -5,4 +5,5 @@ export enum HealthProgramType {
   HingeHealth = 'HingeHealth',
   TeladocHealthDiabetesManagement = 'TeladocHealthDiabetesManagement',
   QuestSelect = 'QuestSelect',
+  TeladocHealthDiabetesPrevention = 'TeladocHealthDiabetesPrevention',
 }
