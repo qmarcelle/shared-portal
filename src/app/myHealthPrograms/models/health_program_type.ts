cfd6b3ec2f4ea4e3dcc12453fee588acd124a618
CS, Navya <Navya_CS@bcbst.com>
2024-10-14 06:19:59 +0000

    Merged PR 3147: US 16297 AbleTo Landing Page
    US 16297 AbleTo Landing Page Changes
    
    [US-16297-AbleTo_Landing_Page.docx](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/3147/attachments/US-16297-AbleTo_Landing_Page.docx)
    
    Related work items: #16297

--- src/app/myHealthPrograms/models/health_program_type.ts
+++ src/app/myHealthPrograms/models/health_program_type.ts
@@ -10,4 +10,5 @@ export enum HealthProgramType {
   TalkToNurse = 'TalkToNurse',
   TeladocHealthGeneralUrgentCare = 'TeladocHealthGeneralUrgentCare',
   TeladocHealthDiabetesPrevention = 'TeladocHealthDiabetesPrevention',
+  AbleTo = 'AbleTo',
 }
