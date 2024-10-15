e3473de7e117cecf45dcc307385583952dcd2277
Rohit Namala <Rohit_Namala@bcbst.com>
2024-10-15 12:00:54 +0000

    Merged PR 3198: Unit Test scripts fix for multiple password text
    Unit Test scripts fix for multiple password text

--- src/tests/app/login/integrations/login_email_verification.spec.tsx
+++ src/tests/app/login/integrations/login_email_verification.spec.tsx
@@ -34,7 +34,9 @@ const setupUI = () => {
   const inputUsername = screen.getByRole('textbox', {
     name: /username/i,
   });
-  const inputPassword = screen.getByLabelText(/password/i);
+  const inputPassword = screen.getByLabelText(/password/i, {
+    selector: 'input',
+  });
   const loginButton = screen.getByRole('button', {
     name: /Log In/i,
   });
