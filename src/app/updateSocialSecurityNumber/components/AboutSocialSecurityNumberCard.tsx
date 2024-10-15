2da76657af01eeda0d00b8e3f2d7a54975e766a2
Utsavi Oza <utsavi_oza@bcbst.com>
2024-10-16 00:13:55 +0530

    Added social security Page.added testcases

---
+++ src/app/updateSocialSecurityNumber/components/AboutSocialSecurityNumberCard.tsx
@@ -0,0 +1,28 @@
+import { Card } from '@/components/foundation/Card';
+import { Column } from '@/components/foundation/Column';
+import { Header } from '@/components/foundation/Header';
+import { Spacer } from '@/components/foundation/Spacer';
+import { TextBox } from '@/components/foundation/TextBox';
+
+export const AboutSocialSecurityNumberCard = () => {
+  return (
+    <Card className="small-section">
+      <Column>
+        <Header className="mb-0 title-2" text="About Social Security Number" />
+        <Spacer size={16} />
+        <TextBox
+          className="body-1 mb-0"
+          text="For privacy and security, we will not display the current social security number we have on file."
+          ariaLabel="For privacy and security, we will not display the current social security number we have on file."
+        ></TextBox>
+        <Spacer size={16} />
+        <TextBox
+          className="body-1 mb-0"
+          text="Your SSN is used to process important tax related documents. Adding or updating your SSN will ensure that your documents are accurate and timely."
+          ariaLabel="Your SSN is used to process important tax related documents. Adding or updating your SSN will ensure that your documents are accurate and timely."
+        ></TextBox>
+        <Spacer size={16} />
+      </Column>
+    </Card>
+  );
+};
---
+++ src/app/updateSocialSecurityNumber/components/MemberListItemCard.tsx
@@ -0,0 +1,70 @@
+import { IComponent } from '@/components/IComponent';
+import { useAppModalStore } from '@/components/foundation/AppModal';
+import { Card } from '@/components/foundation/Card';
+import { Column } from '@/components/foundation/Column';
+import { Divider } from '@/components/foundation/Divider';
+import { alertErrorIcon } from '@/components/foundation/Icons';
+import { Row } from '@/components/foundation/Row';
+import { Spacer } from '@/components/foundation/Spacer';
+import { TextBox } from '@/components/foundation/TextBox';
+import { Title } from '@/components/foundation/Title';
+import Image from 'next/image';
+import editIcon from '../../../../public/assets/edit.svg';
+import { UpdateSocialSecurityNumberJourney } from './journeys/UpdateSocialSecurityNumber';
+
+interface MemberListItemCardProps extends IComponent {
+  memberName: string;
+  DOB: string;
+  isSSN: boolean;
+  icon?: JSX.Element;
+}
+
+export const MemberListItemCard = ({
+  memberName,
+  DOB,
+  className,
+  isSSN,
+  icon = <Image src={editIcon} alt="link" />,
+}: MemberListItemCardProps) => {
+  const { showAppModal } = useAppModalStore();
+  return (
+    <Card className={`cursor-pointer ${className}`} type="elevated">
+      <Column className="m-4">
+        <Spacer size={16} />
+        <TextBox className="ml-2 font-bold title-3" text={memberName} />
+        <TextBox text={'DOB: ' + DOB} className="ml-2" />
+        <Spacer size={16} />
+        <Divider />
+        <Spacer size={16} />
+        {isSSN ? (
+          <TextBox className="ml-2" text="A SSN was found on file." />
+        ) : (
+          <Row>
+            <div className="inline-flex">
+              <Image
+                src={alertErrorIcon}
+                className="icon mr-2"
+                alt={'ErrorIcon'}
+              />
+              <TextBox text="No SSN on file." className="text-red-600" />
+            </div>
+          </Row>
+        )}
+        <Spacer size={16} />
+        <Title
+          className="font-bold primary-color ml-2"
+          text="Update"
+          suffix={icon}
+          callback={() =>
+            showAppModal({
+              content: (
+                <UpdateSocialSecurityNumberJourney memberName={memberName} />
+              ),
+            })
+          }
+        />
+        <Spacer size={16} />
+      </Column>
+    </Card>
+  );
+};
