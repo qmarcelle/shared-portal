6a4055f9940251c397514729bd8a1f6da8aed422
Nasar Theen, Thariq Sharif <Thariq_Sharif_Nasar_Theen@bcbst.com>
2024-10-22 14:08:38 +0000

    Merged PR 3300: claimsAndBenefitsFilterUpdate1
    claimsAndBenefitsFilterUpdate1
    
    Related work items: #19938

--- src/tests/app/benefits/components/BenefitsFilterSection.spec.tsx
+++ src/tests/app/benefits/components/BenefitsFilterSection.spec.tsx
@@ -6,48 +6,43 @@ const renderUI = () => {
   return render(
     <Filter
       className="large-section px-0 m-0"
-      filterHeading="Filter Claims"
+      filterHeading="Filter Benefits"
       filterItems={[
         {
           type: 'dropdown',
           label: 'Member',
           value: [
             {
-              label: 'All Members',
+              label: 'Chris Hall',
               value: '1',
               id: '1',
             },
             {
-              label: 'Chris Hall',
+              label: 'Madission Hall',
               value: '2',
               id: '2',
             },
             {
-              label: 'Madission Hall',
+              label: 'Forest Hall',
               value: '3',
               id: '3',
             },
             {
-              label: 'Forest Hall',
+              label: 'Telly Hall',
               value: '4',
               id: '4',
             },
             {
-              label: 'Telly Hall',
+              label: 'Janie Hall',
               value: '5',
               id: '5',
             },
-            {
-              label: 'Janie Hall',
-              value: '6',
-              id: '6',
-            },
           ],
-          selectedValue: { label: 'All Members', value: '1', id: '1' },
+          selectedValue: { label: 'Chris Hall', value: '1', id: '1' },
         },
         {
           type: 'dropdown',
-          label: 'Claim Type',
+          label: 'Benefit Type',
           value: [
             {
               label: 'All Types',
@@ -74,64 +69,28 @@ const renderUI = () => {
               value: '5',
               id: '5',
             },
-          ],
-          selectedValue: { label: 'All Types', value: '1', id: '1' },
-        },
-        {
-          type: 'dropdown',
-          label: 'Date Range',
-          value: [
             {
-              label: 'Last 30 Days',
-              value: '1',
-              id: '1',
-            },
-            {
-              label: 'Last 60 Days',
-              value: '2',
-              id: '2',
-            },
-            {
-              label: 'Last 90 Days',
-              value: '3',
-              id: '3',
-            },
-            {
-              label: 'Last 120 Days',
-              value: '4',
-              id: '4',
-            },
-            {
-              label: 'Last Calender Year',
-              value: '5',
-              id: '5',
-            },
-            {
-              label: 'Last Two Years',
+              label: 'Other',
               value: '6',
               id: '6',
             },
           ],
-          selectedValue: {
-            label: 'Last Two Years',
-            value: '6',
-            id: '6',
-          },
+          selectedValue: { label: 'All Types', value: '1', id: '1' },
         },
       ]}
     />,
   );
 };
 
-describe('FilterSection', () => {
+describe('BenefitsFilterSection', () => {
   it('should render the UI correctly', async () => {
     const { container } = renderUI();
 
     // Should show selected profile
 
     expect(screen.getByText('Member')).toBeVisible();
-    expect(screen.getByText('Claim Type')).toBeVisible();
-    expect(screen.getByText('Date Range')).toBeVisible();
+    expect(screen.getByText('Benefit Type')).toBeVisible();
+    expect(screen.getByText('Chris Hall')).toBeVisible();
     expect(container).toMatchSnapshot();
 
     // Should show the dropdown when clicked
@@ -139,8 +98,8 @@ describe('FilterSection', () => {
     // Should show all profiles
 
     expect(screen.getByText('Member')).toBeVisible();
-    expect(screen.getByText('Claim Type')).toBeVisible();
-    expect(screen.getByText('Date Range')).toBeVisible();
+    expect(screen.getByText('Benefit Type')).toBeVisible();
+    expect(screen.getByText('Chris Hall')).toBeVisible();
     expect(container).toMatchSnapshot();
   });
 });
