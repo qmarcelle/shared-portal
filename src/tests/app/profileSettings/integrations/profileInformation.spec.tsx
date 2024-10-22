035322d1e2c3172613c60288dfe14c6e2f79835d
CS, Navya <Navya_CS@bcbst.com>
2024-10-22 14:07:31 +0000

    Merged PR 3259: US 25084 Profile Information API Integration
    US 25084 Profile Information API Integration Changes
    
    [US-25084-Profile_Information_API_Integration.docx](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/3259/attachments/US-25084-Profile_Information_API_Integration.docx)
    
    Related work items: #25084

---
+++ src/tests/app/profileSettings/integrations/profileInformation.spec.tsx
@@ -0,0 +1,186 @@
+import { getMemberDetails } from '@/actions/memberDetails';
+import { memberMockResponse } from '@/app/profileSettings/mock/memberMockResponse';
+import ProfileSettingsPage from '@/app/profileSettings/page';
+import { mockedAxios } from '@/tests/__mocks__/axios';
+import '@testing-library/jest-dom';
+import { render, waitFor } from '@testing-library/react';
+
+const setupUI = async () => {
+  const result = await ProfileSettingsPage();
+  render(result);
+};
+
+describe('Profile Information API Integration', () => {
+  test('Profile Information Email API integration  success scenario', async () => {
+    const memberDetails = memberMockResponse;
+    mockedAxios.get.mockResolvedValueOnce({
+      data: {
+        isEmailVerified: false,
+        isPeobExcluded: true,
+        mobileNumber: '',
+        emailAddress: 'navya_cs@bcbst.com',
+        lineOfBusiness: 'REGL',
+        links: {
+          contactUs: 'https://www.bcbst.com/contact-us.page?nav=header',
+          learnMore:
+            'https://www.bcbst.com/about/our-company/corporate-governance/privacy-security/browser-security.page',
+        },
+        dutyToWarn: [
+          {
+            type: 'p',
+            texts: [
+              'By checking this box I agree to enroll in email and/or mobile text communication service, and that I&apos;m a member 18 or older, or the legal guardian or personal representative of a member. BlueCross, its affiliates and its service providers may send me email and/or text communications that also go out to other members at the same time. Unencrypted email or text messages may possibly be intercepted and read by people other than those it&apos;s addressed to. Message and data rates may apply.',
+            ],
+          },
+        ],
+        contactPreferences: [],
+        tierOne: [
+          {
+            communicationCategory: 'TEXT',
+            communicationMethod: 'TEXT',
+            hasTierTwo: false,
+            description: [
+              {
+                type: 'p',
+                texts: ['Get available communications via text'],
+              },
+            ],
+          },
+          {
+            communicationCategory: 'PLIN',
+            communicationMethod: 'EML',
+            hasTierTwo: false,
+            description: [
+              {
+                type: 'h',
+                texts: ['Important Plan Information'],
+              },
+              {
+                type: 'p',
+                texts: [
+                  'We&apos;ll send you details about your coverage including updates to your network, benefits and appeals.',
+                ],
+              },
+            ],
+          },
+          {
+            communicationCategory: 'CLMS',
+            communicationMethod: 'EML',
+            hasTierTwo: false,
+            description: [
+              {
+                type: 'h',
+                texts: ['Claims Information'],
+              },
+              {
+                type: 'p',
+                texts: ['Get notifications about your share of care costs.'],
+              },
+            ],
+          },
+          {
+            communicationCategory: 'HLTW',
+            communicationMethod: 'EML',
+            hasTierTwo: false,
+            description: [
+              {
+                type: 'h',
+                texts: ['Health &amp; Wellness'],
+              },
+              {
+                type: 'p',
+                texts: [
+                  'We&apos;ll send you tips and reminders to help you get the care you need and stay well.',
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    setupUI();
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        `/memberContactPreference?memberKey=${memberDetails.member_ck}&subscriberKey=${memberDetails.subscriber_ck}&getMemberPreferenceBy=memberKeySubscriberKey&memberUserId=${memberDetails.userID}&extendedOptions=true`,
+      );
+    });
+  });
+
+  test('Profile Information Email API integration null scenario', async () => {
+    const memberDetails = memberMockResponse;
+    mockedAxios.get.mockResolvedValueOnce({
+      data: {},
+    });
+
+    setupUI();
+
+    await waitFor(() => {
+      const response = expect(mockedAxios.get).toHaveBeenCalledWith(
+        `/memberContactPreference?memberKey=${memberDetails.member_ck}&subscriberKey=${memberDetails.subscriber_ck}&getMemberPreferenceBy=memberKeySubscriberKey&memberUserId=${memberDetails.userID}&extendedOptions=true`,
+      );
+      expect(response).toBeNull;
+    });
+  });
+
+  test('Profile Information Phone number API integration success scenario', async () => {
+    const memberDetails = await getMemberDetails();
+    const effectiveDetials = new Date().toLocaleDateString(); // current date
+
+    mockedAxios.get.mockResolvedValueOnce({
+      links: [
+        {
+          link: 'https://gtest.js.gdc.bcbst.com/PortalServices/IDCardService/OperationHours',
+          rel: 'Self',
+        },
+        {
+          link: 'https://gtest.js.gdc.bcbst.com/PortalServices/IDCardService/Data',
+          rel: 'Data',
+        },
+        {
+          link: 'https://gtest.js.gdc.bcbst.com/PortalServices/IDCardService/Details',
+          rel: 'Details',
+        },
+        {
+          link: 'https://gtest.js.gdc.bcbst.com/PortalServices/IDCardService/PDF',
+          rel: 'PDF',
+        },
+        {
+          link: 'https://gtest.js.gdc.bcbst.com/PortalServices/IDCardService/Image',
+          rel: 'Image',
+        },
+        {
+          link: 'https://gtest.js.gdc.bcbst.com/PortalServices/IDCardService/Order',
+          rel: 'Order',
+        },
+      ],
+      operationHours: 'M-F 8AM-6PM (EST)',
+      memberServicePhoneNumber: '1-800-565-9140',
+    });
+
+    setupUI();
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        `/IDCardService/OperationHours?groupId=${memberDetails.groupID}&subscriberCk=${memberDetails.subscriber_ck}&effectiveDetials=${effectiveDetials}`,
+      );
+    });
+  });
+
+  test('Profile Information Phone number API integration null scenario', async () => {
+    const memberDetails = await getMemberDetails();
+    const effectiveDetials = new Date().toLocaleDateString(); // current date
+
+    mockedAxios.get.mockResolvedValueOnce({
+      data: {},
+    });
+
+    setupUI();
+
+    await waitFor(() => {
+      const response = expect(mockedAxios.get).toHaveBeenCalledWith(
+        `/IDCardService/OperationHours?groupId=${memberDetails.groupID}&subscriberCk=${memberDetails.subscriber_ck}&effectiveDetials=${effectiveDetials}`,
+      );
+      expect(response).toBeNull;
+    });
+  });
+});
