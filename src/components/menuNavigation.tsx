c7756aca49839b9c6ed009b21f49f206fa67ddc5
Jaganathan <Saravanakumar_Jaganathan@bcbst.com>
2024-11-11 15:40:31 +0530

    Added url to the links in My Plans

--- src/components/menuNavigation.tsx
+++ src/components/menuNavigation.tsx
@@ -140,7 +140,7 @@ const menuNavigation: SiteHeaderSubNavProps[] = [
         description: 'This is Services Used',
         category: 'Plan Details',
         showOnMenu: true,
-        url: '/servicesUsed',
+        url: '/benefits/servicesUsed',
         external: false,
       },
       {
@@ -167,7 +167,7 @@ const menuNavigation: SiteHeaderSubNavProps[] = [
         description: 'This is Submit a Claim',
         category: 'Claims',
         showOnMenu: true,
-        url: '',
+        url: '/claim',
         external: false,
       },
       {
