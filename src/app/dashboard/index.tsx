8ed85f35aa3676976916a0e16f3b70008443f00a
Nachammai Lakshmanan <Nachammai_Lakshmanan@bcbst.com>
2024-11-11 14:42:18 +0530

    US-24317-Fixed-Unit-Test-Case

--- src/app/dashboard/index.tsx
+++ src/app/dashboard/index.tsx
@@ -4,6 +4,7 @@ import { WelcomeBanner } from '@/components/composite/WelcomeBanner';
 import { AlertBar } from '@/components/foundation/AlertBar';
 import { Spacer } from '@/components/foundation/Spacer';
 import { TextBox } from '@/components/foundation/TextBox';
+import { toPascalCase } from '@/utils/pascale_case_formatter';
 import Link from 'next/link';
 import MemberDashboard from './components/MemberDashboard';
 import NonMemberDashboard from './components/NonMemberDashboard';
@@ -25,26 +26,42 @@ const Dashboard = ({ data }: DashboardProps) => {
       <WelcomeBanner
         className="px-4"
         titleText="Welcome, "
-        name={data.currentUser.currUsr?.firstName}
+        name={toPascalCase(data.currentUser.currUsr?.firstName ?? '')}
         minHeight="min-h-[250px]"
         body={
           <>
-            <TextBox
-              text={`Plan: ${data.currentUser.currUsr?.plan?.planName}`}
-            />
-            <Spacer size={8} />
-            <TextBox
-              text={`Subscriber ID: ${data.currentUser.currUsr?.plan?.subId}`}
-            />
-            <Spacer size={8} />
-            <TextBox
-              text={`Group ID: ${data.currentUser.currUsr?.plan?.grpId}`}
-            />
-            <Spacer size={8} />
-            <TextBox
-              text={`Policies: ${data.currentUser.currUsr?.plan?.coverageType?.join(', ')}`}
-            />
-            <Spacer size={16} />
+            {data.currentUser.currUsr?.plan?.planName && (
+              <>
+                <TextBox
+                  text={`Plan: ${data.currentUser.currUsr?.plan?.planName}`}
+                />
+                <Spacer size={8} />
+              </>
+            )}
+            {data.currentUser.currUsr?.plan?.subId && (
+              <>
+                <TextBox
+                  text={`Subscriber ID: ${data.currentUser.currUsr?.plan?.subId}`}
+                />
+                <Spacer size={8} />
+              </>
+            )}
+            {data.currentUser.currUsr?.plan?.grpId && (
+              <>
+                <TextBox
+                  text={`Group ID: ${data.currentUser.currUsr?.plan?.grpId}`}
+                />
+                <Spacer size={8} />
+              </>
+            )}
+            {data.currentUser.currUsr?.plan?.coverageType?.length && (
+              <>
+                <TextBox
+                  text={`Policies: ${data.currentUser.currUsr?.plan?.coverageType?.join(', ')}`}
+                />
+                <Spacer size={16} />
+              </>
+            )}
             <Link href="/myPlan" className="link-white-text">
               <p className="pb-2 pt-2">View Plan Details</p>
             </Link>
