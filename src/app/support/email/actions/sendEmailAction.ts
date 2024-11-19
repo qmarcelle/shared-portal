35aff437f2e969402be54d5d609e42c4250b3bf5
thimmappa_perumalla <thimmappa_perumalla@bcbst.com>
2024-11-19 15:21:52 +0530

    Fixed review comments

--- src/app/support/email/actions/sendEmailAction.ts
+++ src/app/support/email/actions/sendEmailAction.ts
@@ -1,47 +1,10 @@
 'use server';
 
 import { getLoggedInUserInfo } from '@/actions/loggedUserInfo';
-import { getMemberDetails } from '@/actions/memberDetails';
-import { esApi } from '@/utils/api/esApi';
+import { invokePhoneNumberAction } from '@/app/profileSettings/actions/profileSettingsAction';
 import { portalSvcsApi } from '@/utils/api/portalApi';
 import { logger } from '@/utils/logger';
 import { EmailRequest, MemberDetails } from '../models/emalAppData';
-
-export async function invokeEmailAction(): Promise<string> {
-  try {
-    const memberDetails = await getMemberDetails();
-    const emailResponse = await esApi.get(
-      `/memberContactPreference?memberKey=${memberDetails.member_ck}&subscriberKey=${memberDetails.subscriber_ck}&getMemberPreferenceBy=memberKeySubscriberKey&memberUserId=${memberDetails.userID}&extendedOptions=true`,
-    );
-    if (emailResponse?.data?.data?.emailAddress != null) {
-      return emailResponse?.data?.data?.emailAddress;
-    } else {
-      return 'no.email@no_email.com';
-    }
-  } catch (error) {
-    logger.error('Error Response from  Email API', error);
-    throw error;
-  }
-}
-
-export async function invokePhoneNumberAction(): Promise<string> {
-  try {
-    const memberDetails = await getMemberDetails();
-    const effectiveDetials = new Date().toLocaleDateString();
-
-    const phoneNumberResponse = await portalSvcsApi.get(
-      `/IDCardService/OperationHours?groupId=${memberDetails.groupID}&subscriberCk=${memberDetails.subscriber_ck}&effectiveDetials=${effectiveDetials}`,
-    );
-    if (phoneNumberResponse?.data.memberServicePhoneNumber != null) {
-      return phoneNumberResponse?.data.memberServicePhoneNumber;
-    } else {
-      return '1-800-565-9140'; // Phone number fetched from Application Settings(MemberService_PhoneNumber);
-    }
-  } catch (error) {
-    // Add addtional condition if member is Amplify Member then phone number from Application Settings(AHDefault_ServiceNumber which is  1-866-258-3267)
-    return '1-800-565-9140'; // In case of error return default phone number
-  }
-}
 
 export async function invokeFamilyMemberDetailsAction(
   memberCk: string,
@@ -93,15 +56,12 @@ export async function invokeSendEmailAction(
       }
     });
     emailRequest.contactNumber = await invokePhoneNumberAction();
-    console.log('Thimmappa=== Before call');
-    console.log('Before call - ' + JSON.stringify(emailRequest));
+    logger.info('emailRequest - ' + JSON.stringify(emailRequest));
     const resp = await portalSvcsApi.post<string>(
       '/memberservice/api/v1/contactusemail',
       emailRequest,
     );
-
-    console.log('EmailRequest - ' + JSON.stringify(emailRequest));
-    console.log('EmailResponse - ' + JSON.stringify(resp.data));
+    logger.info('EmailResponse - ' + JSON.stringify(resp.data));
   } catch (error) {
     throw error;
   }
