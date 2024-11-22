0e695a51a864a309679522b78cc2465f8b03bf60
CS, Navya <Navya_CS@bcbst.com>
2024-11-22 11:14:03 +0000

    Merged PR 3478: US 25103 Send an Email page UI
    US 25103 Send an Email page UI  changes
    
    [US-25103_Send_Email_UI.docx](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/3478/attachments/US-25103_Send_Email_UI.docx)
    
    ![test_coverage.PNG](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/3478/attachments/test_coverage.PNG)
    
    Related work items: #25103, #36097

---
+++ src/app/support/email/index.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import { GetHelpSection } from '@/components/composite/GetHelpSection';
+import { Column } from '@/components/foundation/Column';
+import { Header } from '@/components/foundation/Header';
+import { Row } from '@/components/foundation/Row';
+import { Spacer } from '@/components/foundation/Spacer';
+import { TextBox } from '@/components/foundation/TextBox';
+import SendEmailForm from './components/SendEmailForm';
+
+const SendAnEmail = () => {
+  return (
+    <main className="flex flex-col justify-center items-center page">
+      <Column className="app-content app-base-font-color">
+        <Column className="md:m-2 m-4">
+          <Row className="justify-between mt-2 md:mt-16 lg:mt-0">
+            <Column className="max-w-2xl ">
+              <Header type="title-1" text="Send an Email" />
+              <Spacer size={8} />
+              <TextBox
+                className="body-1"
+                text="If it's after hours or you'd rather send us an email, we're right here."
+              />
+            </Column>
+          </Row>
+        </Column>
+        <section className="flex flex-row items-start app-body" id="Filter">
+          <Column className="flex-grow page-section-63_33 items-stretch">
+            <SendEmailForm
+              topicsDropdown={[
+                {
+                  label: 'Select',
+                  value: '43',
+                  id: '1',
+                },
+                {
+                  label: 'Benefits & Coverage',
+                  value: '2',
+                  id: '2',
+                },
+                {
+                  label: 'New or Existing Claims',
+                  value: '3',
+                  id: '3',
+                },
+                {
+                  label: 'Deductibles',
+                  value: '4',
+                  id: '4',
+                },
+                {
+                  label: 'Pharmacy & Prescriptions',
+                  value: '5',
+                  id: '5',
+                },
+                {
+                  label: 'Find Care',
+                  value: '6',
+                  id: '6',
+                },
+                {
+                  label: 'Dental',
+                  value: '7',
+                  id: '7',
+                },
+                {
+                  label: 'Membership & Billing Questions',
+                  value: '8',
+                  id: '8',
+                },
+                {
+                  label: 'Other',
+                  value: '9',
+                  id: '9',
+                },
+              ]}
+              onSelectedDateChange={() => {}}
+              selectedtopic={{
+                label: 'Select',
+                value: '43',
+                id: '1',
+              }}
+              nameDropdown={[
+                {
+                  label: 'Chris Hall',
+                  value: '43',
+                  id: '1',
+                },
+                {
+                  label: 'John',
+                  value: '2',
+                  id: '2',
+                },
+              ]}
+              selectedName={{
+                label: 'Chris Hall',
+                value: '43',
+                id: '1',
+              }}
+            />
+          </Column>
+          <Column className=" flex-grow page-section-63_33 items-stretch ">
+            <GetHelpSection
+              link="/support/faq"
+              linkURL="FAQ."
+              headerText="We're Here to Help"
+            />
+          </Column>
+        </section>
+      </Column>
+    </main>
+  );
+};
+
+export default SendAnEmail;
