40c515a3e2ca3f1bd940da454ffa427cbe5fa4f2
braden_carter@bcbst.com <braden_carter@bcbst.com>
2024-11-25 12:31:32 -0500

    Refactoring details page to accomodate widget integrations

---
+++ src/app/benefits/details/details.tsx
@@ -0,0 +1,57 @@
+'use client';
+import { BenefitTypeDetails } from '../components/BenefitTypeDetails';
+import { BenefitTypeDetail } from '../models/benefit_details';
+import { BenefitLevelDetails } from '../models/benefit_type_header_details';
+import { useBenefitsStore } from '../stores/benefitsStore';
+
+export const Details = () => {
+  const { selectedBenefitDetails } = useBenefitsStore();
+
+  // Check if benefitsData is populated
+  if (!selectedBenefitDetails) {
+    return 'No benefits data available.';
+  }
+
+  const benefitTypeDetails: BenefitTypeDetail = { benefitDetails: [] };
+  const benefitLevelDetails: BenefitLevelDetails[] =
+    selectedBenefitDetails.networkTiers.map((tier: NetWorksAndTierInfo) => {
+      return {
+        benefitLevel: tier.tierDesc,
+        benefitValue: tier.tierExplanation,
+      };
+    });
+  benefitTypeDetails.benefitTypeHeaderDetails = {
+    title: selectedBenefitDetails.serviceCategory.category,
+    benefitLevelDetails: benefitLevelDetails,
+  };
+  console.log(benefitLevelDetails);
+  const listCoveredServices = selectedBenefitDetails.coveredServices.map(
+    (service) => {
+      return {
+        listBenefitDetails: service.serviceDetails.map((detail) => {
+          const copayInsurance = detail.memberPays
+            ? `${detail.memberPays}% coinsurance after you pay the deductible`
+            : `$${detail.copay} copay`;
+          return {
+            benefitTitle: detail.description,
+            copayOrCoinsurance: copayInsurance,
+          };
+        }),
+        note: service.serviceDetails[0].comment
+          ? service.serviceDetails[0].comment
+          : '',
+      };
+    },
+  );
+
+  benefitTypeDetails.benefitDetails = listCoveredServices;
+  console.log(benefitTypeDetails.benefitDetails);
+  console.log(`List of covered services: ${listCoveredServices}`);
+  console.log(`BenefitTypeDetails: ${benefitTypeDetails}`);
+
+  return (
+    <main className="flex flex-col justify-center items-center page">
+      <BenefitTypeDetails benefitTypeDetails={benefitTypeDetails} />
+    </main>
+  );
+};
