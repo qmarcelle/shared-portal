06f78f9f92692a1c3cdf3699de1eeea54e6be78e
Nasar Theen, Thariq Sharif <Thariq_Sharif_Nasar_Theen@bcbst.com>
2024-12-02 12:53:19 +0000

    Merged PR 3672: MyHealthPageBlueCareVisibilityRuleChanges
    Related work items: #22692

--- src/app/myHealth/index.tsx
+++ src/app/myHealth/index.tsx
@@ -20,6 +20,7 @@ import healthSupportIcon from '@/public/assets/health_support.svg';
 import healthSurveyIcon from '@/public/assets/health_survey.svg';
 import {
   isBlue365FitnessYourWayEligible,
+  isBlueCareEligible,
   isChipRewardsEligible,
 } from '@/visibilityEngine/computeVisibilityRules';
 import Image from 'next/image';
@@ -158,6 +159,7 @@ export type MyHealthProps = {
   data: MyHealthData;
 };
 const MyHealth = ({ data }: MyHealthProps) => {
+  const isBlueCareMember = isBlueCareEligible(data.visibilityRules);
   return (
     <main className="flex flex-col justify-center items-center page">
       <Column className="app-content app-base-font-color">
@@ -169,37 +171,40 @@ const MyHealth = ({ data }: MyHealthProps) => {
             />
           </section>
         )}
-        <section className="flex flex-row items-start app-body">
-          <Column className="flex-grow page-section-36_67 items-stretch">
-            <PrimaryCareProvider
-              className="large-section"
-              providerDetails={data.primaryCareProvider}
-              label="Primary Care Provider"
-              linkLabel="View or Update Primary Care Provider"
-              title="My Primary Care Provider"
-            />
-          </Column>
-          <Column className="flex-grow page-section-63_33 items-stretch">
-            <Column>
-              <MyHealthOffsiteLinkCard
-                icon={healthSurveyIcon}
-                title="Fill-out The Health History & Needs Survey"
-                description="Help us get a clear picture of your health needs. We need this info once a year from all our members. Please take a few minutes to complete this survey."
-                url={
-                  process.env.NEXT_PUBLIC_FILL_OUT_THE_HEALTH_HISTORY_URL ?? ''
-                }
-              />
-              <MyHealthOffsiteLinkCard
-                icon={healthSupportIcon}
-                title="Get One-on-One Health Support"
-                description="We offer a health program that’s designed just for you. Whether you need support for healthy living or help with a long- or short-term illness or injury, you can rely on us."
-                url={
-                  process.env.NEXT_PUBLIC_ONE_ON_ONE_HEALTH_SUPPORT_URL ?? ''
-                }
+        {isBlueCareMember && (
+          <section className="flex flex-row items-start app-body">
+            <Column className="flex-grow page-section-36_67 items-stretch">
+              <PrimaryCareProvider
+                className="large-section"
+                providerDetails={data.primaryCareProvider}
+                label="Primary Care Provider"
+                linkLabel="View or Update Primary Care Provider"
+                title="My Primary Care Provider"
               />
             </Column>
-          </Column>
-        </section>
+            <Column className="flex-grow page-section-63_33 items-stretch">
+              <Column>
+                <MyHealthOffsiteLinkCard
+                  icon={healthSurveyIcon}
+                  title="Fill-out The Health History & Needs Survey"
+                  description="Help us get a clear picture of your health needs. We need this info once a year from all our members. Please take a few minutes to complete this survey."
+                  url={
+                    process.env.NEXT_PUBLIC_FILL_OUT_THE_HEALTH_HISTORY_URL ??
+                    ''
+                  }
+                />
+                <MyHealthOffsiteLinkCard
+                  icon={healthSupportIcon}
+                  title="Get One-on-One Health Support"
+                  description="We offer a health program that’s designed just for you. Whether you need support for healthy living or help with a long- or short-term illness or injury, you can rely on us."
+                  url={
+                    process.env.NEXT_PUBLIC_ONE_ON_ONE_HEALTH_SUPPORT_URL ?? ''
+                  }
+                />
+              </Column>
+            </Column>
+          </section>
+        )}
         <Spacer size={64} />
         <Header text="Other Programs & Resources" type="title-1" />
         <Spacer size={32} />
@@ -230,28 +235,32 @@ const MyHealth = ({ data }: MyHealthProps) => {
             options={healthLibraryDetails}
           />
         </section>
-        <section>
-          <MyHealthOffsiteLinkCard
-            icon={biometricScreeningIcon}
-            title="Schedule a Biometric Screening"
-            description="We'll help you schedule this important health screening and walk you through the steps to prepare for your doctor visit."
-            url=""
-          />
-        </section>
-        <section>
-          <MemberWellnessCenterOptions
-            className="large-section"
-            options={memberWellnessCenterDetails}
-          />
-        </section>
-        <section>
-          <MyHealthOffsiteLinkCard
-            icon={dentalHealthLibraryIcon}
-            title="Dental Health Library"
-            description="Check dental symptoms and get your dental questions answered."
-            url=""
-          />
-        </section>
+        {!isBlueCareMember && (
+          <>
+            <section>
+              <MyHealthOffsiteLinkCard
+                icon={biometricScreeningIcon}
+                title="Schedule a Biometric Screening"
+                description="We'll help you schedule this important health screening and walk you through the steps to prepare for your doctor visit."
+                url=""
+              />
+            </section>
+            <section>
+              <MemberWellnessCenterOptions
+                className="large-section"
+                options={memberWellnessCenterDetails}
+              />
+            </section>
+            <section>
+              <MyHealthOffsiteLinkCard
+                icon={dentalHealthLibraryIcon}
+                title="Dental Health Library"
+                description="Check dental symptoms and get your dental questions answered."
+                url=""
+              />
+            </section>
+          </>
+        )}
       </Column>
     </main>
   );
