0ef09e5fcf3f29ecd63edd4b609ebc41f8d07d85
Nasar Theen, Thariq Sharif <Thariq_Sharif_Nasar_Theen@bcbst.com>
2024-12-02 12:53:19 +0000

    Merged PR 3672: MyHealthPageBlueCareVisibilityRuleChanges
    Related work items: #22692

---
+++ src/tests/app/myHealth/MyHealth.spec.tsx
@@ -0,0 +1,75 @@
+import MyHealthPage from '@/app/myHealth/page';
+import { mockedAxios } from '@/tests/__mocks__/axios';
+import { UserRole } from '@/userManagement/models/sessionUser';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+const renderUI = async () => {
+  const page = await MyHealthPage();
+  return render(page);
+};
+
+jest.mock('../../../auth', () => ({
+  auth: jest.fn(() =>
+    Promise.resolve({
+      user: {
+        currUsr: {
+          firstName: 'Chris',
+          role: UserRole.MEMBER,
+          plan: {
+            memCk: '123456789',
+          },
+        },
+        vRules: {
+          futureEffective: false,
+          fsaOnly: false,
+          wellnessOnly: false,
+          terminated: false,
+          katieBeckNoBenefitsElig: false,
+          blueCare: false,
+        },
+      },
+    }),
+  ),
+}));
+
+describe('My Health Page', () => {
+  it('should render My Health Page correctly', async () => {
+    mockedAxios.get.mockResolvedValueOnce({
+      data: {
+        physicianId: '3118777',
+        physicianName: 'Louthan, James D.',
+        address1: '2033 Meadowview Ln Ste 200',
+        address2: '',
+        address3: '',
+        city: 'Kingsport',
+        state: 'TN',
+        zip: '376607432',
+        phone: '4238572260',
+        ext: '',
+        addressType: '1',
+        taxId: '621388079',
+      },
+    });
+    const component = await renderUI();
+    expect(mockedAxios.get).toHaveBeenCalledWith(
+      '/memberservice/PCPhysicianService/pcPhysician/123456789',
+    );
+
+    screen.getByText('WellTuned Blog');
+    screen.getByText(
+      'Visit our WellTuned blog to stay up-to-date on health and wellness news, health care developments and tips for managing your health.',
+    );
+    expect(screen.getByText('Health Library')).toBeVisible();
+    expect(screen.getByText('Visit The Health Library')).toBeVisible();
+    expect(screen.getByText('Schedule a Biometric Screening')).toBeVisible();
+    expect(screen.getByText('Member Wellness Center')).toBeVisible();
+    expect(
+      screen.getByText(
+        'Take a free personal health assessment, track your diet and exercise, sync your fitness apps to earn wellness points and more—all in one secure place.',
+      ),
+    ).toBeVisible();
+    expect(screen.getByText('Visit Member Wellness Center')).toBeVisible();
+    expect(component.baseElement).toMatchSnapshot();
+  });
+});
