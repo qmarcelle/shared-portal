64feef0a4673fecca8c1e3c150d697dfdb6682d8
Rohit Namala <Rohit_Namala@bcbst.com>
2024-12-11 12:09:38 +0000

    Merged PR 3677: Pharmacy docs and forms 35500
    Added formulary pdfs as assets which show up as per the group and other factors of the user.
    
    Related work items: #35500

---
+++ src/app/pharmacy/actions/getFormularyFilter.tsx
@@ -0,0 +1,49 @@
+import { logger } from '@/utils/logger';
+
+export async function getFormularyFilter(
+  memberDetails: any,
+): Promise<string | null> {
+  try {
+    const memberInfo = memberDetails;
+
+    const MEDICARE_MAPD_BLUEADVANTAGE_GROUP_ABADTX00_PLAN_ID = 'ABADTX00';
+    const MEDICARE_MAPD_BLUEADVANTAGE_GROUP = '116884';
+
+    const isBlueAdvantageExtraFormularyEnabled = (memberInfo: any) => {
+      if (memberInfo == null) return false;
+      return (
+        MEDICARE_MAPD_BLUEADVANTAGE_GROUP === memberInfo.groupID &&
+        MEDICARE_MAPD_BLUEADVANTAGE_GROUP_ABADTX00_PLAN_ID ===
+          memberInfo.mPDPD_ID
+      );
+    };
+
+    const sortByGroup =
+      '87898,115617,129970,129969,130300,120970,115602,128361,119381,118069,122059,124377,115613,118068,121489,132570,116884,129884,129971,145130,142512,142513';
+    if (
+      sortByGroup != null &&
+      sortByGroup.split(',').includes(memberInfo.groupID)
+    ) {
+      if (memberInfo.groupID === '87898') {
+        return 'Preferred';
+      } else if (isBlueAdvantageExtraFormularyEnabled(memberInfo)) {
+        return (
+          memberInfo.groupID +
+          '-' +
+          MEDICARE_MAPD_BLUEADVANTAGE_GROUP_ABADTX00_PLAN_ID
+        );
+      } else {
+        return memberInfo.groupID;
+      }
+    } else if (memberInfo.rxEssentialEligible) {
+      return 'Essential';
+    } else if (memberInfo.rxEssentialPlusEligible) {
+      return 'EssentialPlus';
+    } else if (memberInfo.rxPreferredEligible) {
+      return 'Preferred';
+    } else return 'null';
+  } catch (error) {
+    logger.error('Drug list formulary', error);
+    return '';
+  }
+}
