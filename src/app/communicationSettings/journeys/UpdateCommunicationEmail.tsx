7e95f7021bd9c1f77d58417835c2569b7db174b2
Kamidi, Suryakumari <Suryakumari_Kamidi@bcbst.com>
2024-12-18 06:16:50 +0000

    Merged PR 3870: US-35808 Updating emails in portal with email uniqueness rules
    Handled Error scenario for Communication settings page on clicking on email update link
    
    Related work items: #35808

--- src/app/communicationSettings/journeys/UpdateCommunicationEmail.tsx
+++ src/app/communicationSettings/journeys/UpdateCommunicationEmail.tsx
@@ -7,12 +7,23 @@ import {
   useAppModalStore,
 } from '@/components/foundation/AppModal';
 import { Column } from '@/components/foundation/Column';
+import { Row } from '@/components/foundation/Row';
 import { Spacer } from '@/components/foundation/Spacer';
 import { TextBox } from '@/components/foundation/TextBox';
 import { TextField } from '@/components/foundation/TextField';
-import { useState } from 'react';
+import alertErrorSvg from '@/public/assets/alert_error_red.svg';
+import {
+  isValidEmailAddress,
+  isValidEmailDomain,
+  validateLength,
+} from '@/utils/inputValidator';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { invokeUpdateEmailAddress } from '../actions/emailUniquenessAction';
+import { EmailUniquenessRequest } from '../models/api/emailUniqueness';
 
 const headerText = 'Confirm Email Address';
+
 interface UpdateCommunicationEmailProps {
   email: string;
 }
@@ -23,25 +34,117 @@ export const UpdateCommunicationEmail = ({
   email,
 }: ModalChildProps & UpdateCommunicationEmailProps) => {
   const { dismissModal } = useAppModalStore();
-  /* const initChange = () => {
-    changePage!(1, true);
-  };
-  function changePageIndex(index: number, showback = true) {
-    changePage?.(index, showback);
-  } */
   const [mainAuthDevice, setMainAuthDevice] = useState(email);
-  const [newAuthDevice, setNewAuthDevice] = useState('');
+  const [newAuthDevice, setNewAuthDevice] = useState(email);
+  const [confirmEmail, setConfirmEmail] = useState('');
   const [confirmCode, setConfirmCode] = useState('');
+  const [showConfirmEmail, setShowConfirmEmail] = useState(false);
+  const [error, setError] = useState('');
+  const [nextDisabled, setNextDisabled] = useState(false);
+
+  useEffect(() => {
+    if (pageIndex === 0) {
+      setNewAuthDevice(email);
+      setConfirmEmail('');
+      setConfirmCode('');
+      setShowConfirmEmail(false);
+      setError('');
+      setNextDisabled(false);
+    }
+  }, [pageIndex, email]);
 
   const initNewDevice = async () => {
-    // Do API call for new device
-    setMainAuthDevice(newAuthDevice);
-    changePage?.(1, true);
+    try {
+      let emailToUse = newAuthDevice;
+      if (newAuthDevice === '') {
+        emailToUse = mainAuthDevice;
+        setNewAuthDevice(mainAuthDevice);
+      } else {
+        setMainAuthDevice(newAuthDevice);
+      }
+      const emailRequest: EmailUniquenessRequest = {
+        emailAddress: emailToUse,
+        memberKey: '',
+        subscriberKey: '',
+      };
+      const response = await invokeUpdateEmailAddress(emailRequest);
+      if (response.errorCode === 'RE-400-17') {
+        setError(
+          'The email address entered is already in use by another account. Please choose a different email address.',
+        );
+        setNextDisabled(true); // Disable the Next button if there's an error
+      } else {
+        changePage?.(1, true);
+        setShowConfirmEmail(false);
+        setNextDisabled(false); // Enable the Next button if the email is valid
+      }
+    } catch (errorMessage: unknown) {
+      console.error(
+        'error in emailUniqueness for updating the email',
+        errorMessage,
+      );
+      setNextDisabled(true); // Disable the Next button if there's an error
+    }
   };
 
   const submitCode = async () => {
     // Do API call for submit code
     changePage?.(2, false);
+  };
+
+  const validateEmailAddress = (value: string) => {
+    setNewAuthDevice(value);
+    setNextDisabled(false); // Enable the Next button when user starts typing
+    const isValidEmail = isValidEmailAddress(value);
+    const isValidLength = validateLength(value);
+
+    if (!isValidEmail || !isValidLength) {
+      setError('Please enter a valid address.');
+      setShowConfirmEmail(false);
+      return;
+    }
+
+    if (value === '') {
+      setError('');
+      setShowConfirmEmail(false);
+      return;
+    }
+
+    if (isValidEmailDomain(value)) {
+      setShowConfirmEmail(true);
+      setError('');
+    } else {
+      setShowConfirmEmail(false);
+      setError(
+        'The email address you provided is invalid or it’s from a domain we don’t allow. Please choose another email address.',
+      );
+    }
+  };
+
+  const handleConfirmEmailChange = (val: string) => {
+    setConfirmEmail(val);
+    if (val === '') {
+      setError('');
+    } else if (val !== newAuthDevice) {
+      setError('The email addresses must match. Please check and try again.');
+    } else {
+      setError('');
+    }
+  };
+
+  const getNextCallback = (
+    newAuthDevice: string,
+    confirmEmail: string,
+    initNewDevice: () => void,
+  ): (() => void) | undefined => {
+    if (
+      isValidEmailAddress(newAuthDevice) &&
+      isValidEmailDomain(newAuthDevice) &&
+      newAuthDevice === confirmEmail
+    ) {
+      return () => initNewDevice();
+    }
+    return undefined;
   };
 
   const pages = [
@@ -50,14 +153,38 @@ export const UpdateCommunicationEmail = ({
       label="Update Email Address"
       subLabel="Enter the new email address you'd like to use for communications and security settings."
       actionArea={
-        <TextField
-          valueCallback={(val) => setNewAuthDevice(val)}
-          label="Email Address"
-        />
+        <Column>
+          <TextField
+            value={newAuthDevice}
+            valueCallback={(val) => validateEmailAddress(val)}
+            label="Email Address"
+            type="email"
+          />
+          {showConfirmEmail && (
+            <Column>
+              <Spacer size={32} />
+              <TextField
+                value={confirmEmail}
+                valueCallback={handleConfirmEmailChange}
+                label="Confirm Email"
+              />
+            </Column>
+          )}
+          {error && (
+            <div className="text-red-500 mt-2">
+              <Row>
+                <Image src={alertErrorSvg} className="icon mt-1" alt="alert" />
+                <TextBox className="body-1 pt-1.5 ml-2" text={error} />
+              </Row>
+            </div>
+          )}
+        </Column>
       }
       cancelCallback={() => dismissModal()}
       nextCallback={
-        newAuthDevice.length > 5 ? () => initNewDevice() : undefined
+        nextDisabled
+          ? undefined
+          : getNextCallback(newAuthDevice, confirmEmail, initNewDevice)
       }
     />,
     <InputModalSlide
@@ -73,9 +200,10 @@ export const UpdateCommunicationEmail = ({
           <Spacer size={32} />
           <TextField
             type="text"
+            value={confirmCode}
             valueCallback={(val) => setConfirmCode(val)}
             label="Enter Security Code"
-          ></TextField>
+          />
           <Spacer size={16} />
           <AppLink className="self-start" label="Resend Code" />
           <Spacer size={32} />
