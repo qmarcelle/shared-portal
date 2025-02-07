8a2fc760f638257a0f3168ea64e9e2b13110f9f5
Jaganathan <Saravanakumar_Jaganathan@bcbst.com>
2025-02-07 18:40:57 +0530

    Added SSO link to CVS caremark

--- src/app/pharmacy/components/CVSCareMarkCards.tsx
+++ src/app/pharmacy/components/CVSCareMarkCards.tsx
@@ -2,6 +2,7 @@ import { Card } from '@/components/foundation/Card';
 import { Row } from '@/components/foundation/Row';
 import { TextBox } from '@/components/foundation/TextBox';
 import { IComponent } from '@/components/IComponent';
+import { useRouter } from 'next/navigation';
 import { CVSCareMarkDetails } from '../models/caremark_details';
 
 interface CVSCaremarkCardProps extends IComponent, CVSCareMarkDetails {}
@@ -10,9 +11,17 @@ export const CVSCaremarkCard = ({
   className,
   serviceIcon,
   serviceLabel,
+  url,
 }: CVSCaremarkCardProps) => {
+  const router = useRouter();
   return (
-    <Card className={`cursor-pointer ${className}`} type="elevated">
+    <Card
+      className={`cursor-pointer ${className}`}
+      type="elevated"
+      onClick={() => {
+        if (url) router.push(url);
+      }}
+    >
       <Row className="p-2">
         {serviceIcon}
         <TextBox
