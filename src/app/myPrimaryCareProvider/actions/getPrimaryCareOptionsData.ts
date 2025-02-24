c5e32629f2da9b2ede44ccaa2eb7a4bc75dbf036
Rohit Namala <R16633N@bcbst.com>
2025-02-24 10:43:44 +0000

    Merged PR 4897: PCPInfo build issue fix
    PCPInfo build issue fix
    
    Related work items: #18879

--- src/app/myPrimaryCareProvider/actions/getPrimaryCareOptionsData.ts
+++ src/app/myPrimaryCareProvider/actions/getPrimaryCareOptionsData.ts
@@ -4,16 +4,18 @@ import { getLoggedInUserInfo } from '@/actions/loggedUserInfo';
 import { PrimaryCareOptionsData } from '@/app/findcare/primaryCareOptions/model/app/primary_care_options_data';
 import { auth } from '@/auth';
 import { ActionResponse } from '@/models/app/actionResponse';
+import { logger } from '@/utils/logger';
 import { getDependentPCPInfo, getPCPProviderInfo } from './getPCPInfo';
 
 export const getPrimaryCareOptionsData = async (): Promise<
   ActionResponse<number, PrimaryCareOptionsData>
 > => {
-  const session = await auth();
-  const loggedUserInfo = await getLoggedInUserInfo(
-    session?.user.currUsr?.plan.memCk ?? '',
-  );
+  
   try {
+    const session = await auth();
+    const loggedUserInfo = await getLoggedInUserInfo(
+    session!.user.currUsr!.plan!.memCk,
+  );
     const pcpPhysician = await getPCPProviderInfo(loggedUserInfo, session);
     const depPcpPhysician = await getDependentPCPInfo(loggedUserInfo, session);
     return {
@@ -25,11 +27,11 @@ export const getPrimaryCareOptionsData = async (): Promise<
       },
     };
   } catch (error) {
+    logger.error('Primary Care Provider API - Failure', error);
     return {
       status: 400,
       data: {
         primaryCareProvider: null,
-        visibilityRules: session?.user.vRules,
       },
     };
   }
