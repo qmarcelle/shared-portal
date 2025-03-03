99a650c5383ca5ae979fa6aea98e8a752f5bcd25
Kamidi, Suryakumari <Suryakumari_Kamidi@bcbst.com>
2025-03-03 08:48:15 +0000

    Merged PR 4103: US-38298 Communications Settings Page (Backend)
    If I log into portal and go to the subscriber name in the header (right corner), I should see Communciations Settings menu items appear. If I click on this menu item, I should arrive at the Communication Settings page where I see the following components:
    Contact information (includes phone number and email address on file, both editable)
    Request Print Material (includes a link to start a chat)
    Edit Alert Preferences (includes radio buttons for Receive Text Alerts and Receive Email Alerts)
    AND if I click on either of the radio buttons the Next button becomes active
    AND if I click on Receive Email Alerts then three additional radio buttons appear:
    Important Plan Information
    Claims Information
    Health & Wellness
    AND selections from Receive Text Alerts and Receive Email Alerts should be written to the MECM table.
    
    Related work items: #38298

--- src/app/communicationSettings/journeys/UpdateCommunicationTerms.tsx
+++ src/app/communicationSettings/journeys/UpdateCommunicationTerms.tsx
@@ -7,38 +7,55 @@ import {
 import { Column } from '@/components/foundation/Column';
 import { Spacer } from '@/components/foundation/Spacer';
 import { TextBox } from '@/components/foundation/TextBox';
+import { saveDataAction } from '../actions/communicationSettingsAction';
 
-interface UpdateCommunicationTermsProps {}
+interface UpdateCommunicationTermsProps {
+  selectedPreferences: {
+    memberKey: string | undefined;
+    subscriberKey: string | undefined;
+    emailAddress: string;
+    mobileNumber: string;
+    groupKey: string;
+    lineOfBusiness: string;
+    contactPreference: {
+      optOut: string;
+      communicationCategory: string;
+      communicationMethod: string;
+    }[];
+    dutyToWarn: string[];
+  };
+}
 
 export const UpdateCommunicationTerms = ({
   changePage,
   pageIndex,
+  selectedPreferences,
 }: ModalChildProps & UpdateCommunicationTermsProps) => {
   const { dismissModal } = useAppModalStore();
-  /* const [mainAuthDevice, setMainAuthDevice] = useState('');
-  const [newAuthDevice, setNewAuthDevice] = useState('');
-  const [confirmCode, setConfirmCode] = useState(''); */
 
-  /* const initNewDevice = async () => {
-    // Do API call for new device
-    setMainAuthDevice(newAuthDevice);
-    changePage?.(1, true);
+  const handleNext = async () => {
+    try {
+      console.log('selectedPreferences', selectedPreferences);
+      const response = await saveDataAction(selectedPreferences);
+
+      if (response.data?.componentStatus === 'Success') {
+        changePage?.(1, true);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
-
-  const submitCode = async () => {
-    // Do API call for submit code
-    changePage?.(2, false);
-  }; */
-
   const pages = [
     <ConfirmTermsSlide
       key={0}
       label="Confirm Changes"
       subLabel="Agree to the terms below and change your contact preferences."
       linkLabel="Cancel"
-      checkboxLabel="By checking the box, I agree to enroll in email and/or mobile communication service as a member that is 18 or older, or as the legal guardian or personal representative of a member. I understand that communications via unencrypted internet and/or via text message are not secure and that there is a possibility that information included in email and/or text messages can be intercepted and read by other parties besides the person to whom it is addressed. By signing up, I accept the risks associated with emailing and text messaging and understand that message and data rates may apply. Further, by providing a phone number, I agree that BlueCross, its affiliates, and service providers may contact me using automated dialing systems."
+      checkboxLabel={selectedPreferences.dutyToWarn
+        .join('')
+        .replaceAll('&apos;', '′')}
       cancelCallback={() => dismissModal()}
-      nextCallback={() => changePage?.(1, true)}
+      nextCallback={handleNext}
     />,
     <SuccessSlide
       key={1}
