193daa7c65b51b6101b4a5805511c47204a507f6
Oza, Utsavi <Utsavi_Oza@bcbst.com>
2025-03-06 13:53:47 +0000

    Merged PR 5008: UserStory-44315 1095 TaxDocumentPage with No Form
    Created page 1095BFormRequest Page
    Created FormRequestCard Component to be rendered inside 1095BFormRequest Page.
    Covered Testcase for 1095BFormRequest Page
    Testing Document : [EOT_US_44315.docx](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/5008/attachments/EOT_US_44315.docx)
    
    Related work items: #44315

---
+++ src/tests/app/1095BFormRequest/FormRequestPage.spec.tsx
@@ -0,0 +1,21 @@
+import RequestFormPage from '@/app/1095BFormRequest/page';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+const renderUI = async () => {
+  const page = await RequestFormPage();
+  return render(page);
+};
+
+describe('1095BFormRequest Page', () => {
+  it('should render FormRequest Page UI correctly', async () => {
+    const component = await renderUI();
+    expect(screen.getByText('Request a 1095-B Form')).toBeVisible();
+    expect(
+      screen.getByText(
+        'We’re sorry, but we don’t supply a 1095-B form for the type of coverage you have.',
+      ),
+    ).toBeVisible();
+    expect(component.baseElement).toMatchSnapshot();
+  });
+});
