68471ae50930ee16718f4e6eb1fe1a5eebad6b4d
Kamidi, Suryakumari <Suryakumari_Kamidi@bcbst.com>
2025-03-13 07:03:56 +0000

    Merged PR 5079: US- 41411 Type of Plan Component - My Plan's Plan Details (backend)
    In My Plan page on click of view who's covered by using loggedInUserInfo api displayed plandetails information along with name and date of birth
    
    on click of view plan contact information using contact api displayed the phone number and address has been displayed from loggedInUserInfo
    
    Related work items: #41411

---
+++ src/app/myPlan/actions/getAllPlansData.ts
@@ -0,0 +1,52 @@
+'use server';
+
+import { getMemberAndDependentsPlanDetails } from '@/actions/memberDetails';
+import { auth } from '@/auth';
+import { ActionResponse } from '@/models/app/actionResponse';
+import { portalSvcsApi } from '@/utils/api/portalApi';
+import { logger } from '@/utils/logger';
+import {
+  ContactInfoData,
+  ContactInfoResponse,
+} from '../model/app/contactInfoResponse';
+import { AllMyPlanData } from '../model/app/myPlanData';
+
+export const getAllPlansData = async (): Promise<
+  ActionResponse<number, AllMyPlanData[]>
+> => {
+  const session = await auth();
+  try {
+    const membersData = await getMemberAndDependentsPlanDetails(session);
+    return {
+      status: 200,
+      data: membersData,
+    };
+  } catch (error) {
+    return {
+      status: 400,
+      data: [],
+    };
+  }
+};
+
+export async function getContactInfo(umpi: string): Promise<ContactInfoData> {
+  try {
+    const resp = await portalSvcsApi.get<ContactInfoResponse>('/contact', {
+      params: {
+        umpi,
+      },
+    });
+
+    return resp.data.data;
+  } catch (err) {
+    logger.error('Contact Api Failed', err);
+    return {
+      email: 'demo@bcbst.com',
+      email_verified_flag: true,
+      phone: '7654387656',
+      phone_verified_flag: true,
+      umpi: 'pool5',
+    };
+    //throw err;
+  }
+}
--- src/app/myPlan/model/app/myPlanData.ts
+++ src/app/myPlan/model/app/myPlanData.ts
@@ -1,7 +1,32 @@
+import { Address } from '@/models/member/api/loggedInUserInfo';
 import { VisibilityRules } from '@/visibilityEngine/rules';
+
+export type PlanDetail = {
+  productCategory: string;
+  planID: string;
+  effectiveDate: number;
+  planStartDate: number;
+  planClassID: string;
+  networkPlanName: string;
+  isMedical: boolean;
+  isDental: boolean;
+  isVision: boolean;
+  isSupplemental: boolean;
+};
 
 export type MyPlanData = {
   idCardSvgFrontData: string | null;
   planType: string | null;
   visibilityRules?: VisibilityRules;
+};
+
+export type AllMyPlanData = {
+  memberName: string;
+  dob: string;
+  planDetails: PlanDetail[];
+  medicalEffectiveDate: string;
+  dentalEffectiveDate: string;
+  visionEffectiveDate: string;
+  address: Address[];
+  primaryPhoneNumber: string;
 };
