34168b61609f1080d6e17f40c44e11f5091b9d54
CS, Navya <Navya_CS@bcbst.com>
2025-04-02 05:21:59 +0000

    Merged PR 5372: US 41413 Plan Contact Information API Integration
    As a member, I should see mailing address and phone number associated with my BCBST plan from the Plan Contact Information page
    
    ![coverage.PNG](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/5372/attachments/coverage.PNG)
    
    Related work items: #41413

--- src/app/myPlan/planContactInformation/components/PlanContactCard.tsx
+++ src/app/myPlan/planContactInformation/components/PlanContactCard.tsx
@@ -7,19 +7,21 @@ import { Spacer } from '../../../../components/foundation/Spacer';
 import { TextBox } from '../../../../components/foundation/TextBox';
 
 interface PlanContactCardProps extends IComponent {
-  memberName: string;
-  DOB: string;
-  age: number;
-  phone: string;
+  name: string;
+  dob: string;
+  age: number | undefined;
+  primaryPhoneNumber: string;
+  secondaryPhoneNumber: string;
   address: string;
 }
 
 export const PlanContactCard = ({
-  memberName,
-  DOB,
+  name,
+  dob,
   className,
   age,
-  phone,
+  primaryPhoneNumber,
+  secondaryPhoneNumber,
   address,
 }: PlanContactCardProps) => {
   return (
@@ -27,31 +29,42 @@ export const PlanContactCard = ({
       <Column className="m-4">
         <Spacer size={16} />
         <Row className="justify-between">
-          <TextBox className="ml-2 font-bold body-1" text={memberName} />
+          <TextBox className="ml-2 font-bold body-1" text={name} />
         </Row>
-        <Spacer size={16} />
         <Row className="ml-2">
-          <TextBox text={'DOB: ' + DOB} />
+          <TextBox text={'DOB: ' + dob} />
         </Row>
-        <Spacer size={16} />
+        <Spacer size={8} />
         <Row>
           <Spacer axis="horizontal" size={8} />
           <Divider />
         </Row>
         <Spacer size={16} />
-        {age > 12 ? (
+        {age && age > 12 ? (
           <>
             <TextBox text="Mailing Address" className="ml-2" type="body-2" />
             <Spacer size={1} />
             <TextBox text={address} className="ml-2 w-52" />
             <Spacer size={5} />
-            <TextBox text="Phone" type="body-2" className="ml-2" />
-            <Spacer size={1} />
-            <TextBox text={phone} className="ml-2" />
+            <TextBox
+              text="Primary Phone Number"
+              type="body-2"
+              className="ml-2"
+            />
+            <Spacer size={5} />
+            <TextBox text={primaryPhoneNumber} className="ml-2" />
+            <Spacer size={5} />
+            <TextBox
+              text="Secondary Phone Number"
+              type="body-2"
+              className="ml-2"
+            />
+            <Spacer size={5} />
+            <TextBox text={secondaryPhoneNumber} className="ml-2" />
           </>
         ) : (
           <TextBox
-            className="ml-2 body-1"
+            className="ml-2 body-1 neutral card-main p-4"
             text="This dependent is under age 13. Per our policy, all communications and information for them will be sent to the subscriber unless an exception is approved."
           />
         )}
