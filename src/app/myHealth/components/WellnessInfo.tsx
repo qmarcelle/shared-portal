f96a10cfc3ddf8364191ea5415622ebe036b4b27
Oza, Utsavi <Utsavi_Oza@bcbst.com>
2025-04-04 12:26:26 +0000

    Merged PR 5972: UserStory- 42478 WellnessRewards SelfFunded Employer UI
    created reuseable component WellnessInfo card and used it in wellness reward component .
    wrote unittestcases.
    [EOT_US_42478.docx](https://dev.azure.com/BCBST-EIT/fe7cff4a-a3c1-469b-ac72-6e703e14165d/_apis/git/repositories/55651948-8e16-43b4-bea8-883264cbe60f/pullRequests/5972/attachments/EOT_US_42478.docx)
    
    Related work items: #42478

---
+++ src/app/myHealth/components/WellnessInfo.tsx
@@ -0,0 +1,46 @@
+import { Button } from '@/components/foundation/Button';
+import { Spacer } from '@/components/foundation/Spacer';
+import { TextBox } from '@/components/foundation/TextBox';
+import Image from 'next/image';
+import { IComponent } from '../../../components/IComponent';
+import { Card } from '../../../components/foundation/Card';
+import { Column } from '../../../components/foundation/Column';
+import { Header } from '../../../components/foundation/Header';
+import { externalIcon } from '../../../components/foundation/Icons';
+
+interface WellnessInfoProps extends IComponent {
+  header: string;
+  subHeader: string;
+  bodyText: string;
+  buttonText: string;
+}
+
+export const WellnessInfo = ({
+  className,
+  header,
+  subHeader,
+  bodyText,
+  buttonText,
+}: WellnessInfoProps) => {
+  return (
+    <>
+      <Spacer size={32} />
+      <Header text={header} />
+      <Card className={className}>
+        <Column>
+          <Header type="title-2" text={subHeader} />
+          <Spacer size={25} />
+          <TextBox text={bodyText} />
+          <Spacer size={18} />
+          <Button
+            type="secondary"
+            className="w-1/5"
+            label={buttonText}
+            icon={<Image alt="external icon" src={externalIcon} />}
+            callback={() => null}
+          />
+        </Column>
+      </Card>
+    </>
+  );
+};
