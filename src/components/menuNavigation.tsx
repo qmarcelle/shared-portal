55260e1fbe3db09f90ba672b20d27210d46649e6
Micah Wright <Micah_Wright@bcbst.com>
2025-04-08 13:55:33 -0400

    fixed changes with dental library

--- src/components/menuNavigation.tsx
+++ src/components/menuNavigation.tsx
@@ -238,22 +238,9 @@ export const getMenuNavigation = (
         title: 'Submit a Claim',
         description: 'This is Submit a Claim',
         category: 'Claims',
-        showOnMenu: () => {
-          return true;
-        },
+        showOnMenu: isNotWellnessQa,
         url: '/claims/submitAClaim',
         external: false,
-      },
-      {
-        id: 79,
-        title: 'Health Library',
-        description: 'This is Health Library',
-        category: 'Advice & Support',
-        showOnMenu: () => {
-          return true;
-        },
-        url: 'https://www.healthwise.net/bcbst/Content/CustDocument.aspx?XML=STUB.XML&XSL=CD.FRONTPAGE.XSL&sv=881d5daa-1051-f477-21f5-a05f3e6cdf78',
-        external: true,
       },
       {
         id: 78,
@@ -428,15 +415,6 @@ export const getMenuNavigation = (
         external: false,
       },
       {
-        id: 80,
-        title: 'Dental Health Library',
-        description: 'This is Dental Health Library',
-        category: 'Advice & Support',
-        showOnMenu: isNotWellnessQa,
-        url: 'https://bcbstwelltuned.com/',
-        external: true,
-      },
-      {
         id: 79,
         title: 'Health Library',
         description: 'This is Health Library',
@@ -447,7 +425,6 @@ export const getMenuNavigation = (
         url: 'https://www.healthwise.net/bcbst/Content/CustDocument.aspx?XML=STUB.XML&XSL=CD.FRONTPAGE.XSL&sv=881d5daa-1051-f477-21f5-a05f3e6cdf78',
         external: true,
       },
-
       {
         id: 78,
         title: 'WellTuned Blog',
