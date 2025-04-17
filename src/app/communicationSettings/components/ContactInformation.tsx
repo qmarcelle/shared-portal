a5bd2432fc587ff8f19342b2bd1a8d082bef5f2f
CS, Navya <Navya_CS@bcbst.com>
2025-04-17 12:18:56 +0000

    Merged PR 6267: US 58803 Error Handling for communication settings page
    US 58803 Error Handling for communication settings page
    
    Related work items: #58803

--- src/app/communicationSettings/components/ContactInformation.tsx
+++ src/app/communicationSettings/components/ContactInformation.tsx
@@ -1,8 +1,10 @@
 import { UpdateCommunicationEmail } from '@/app/communicationSettings/journeys/UpdateCommunicationEmail';
 import { UpdateCommunicationText } from '@/app/communicationSettings/journeys/UpdateCommunicationText';
 import { IComponent } from '@/components/IComponent';
+import { ErrorInfoCard } from '@/components/composite/ErrorInfoCard';
 import { useAppModalStore } from '@/components/foundation/AppModal';
 import { Card } from '@/components/foundation/Card';
+import { Column } from '@/components/foundation/Column';
 import { Header } from '@/components/foundation/Header';
 import { Spacer } from '@/components/foundation/Spacer';
 import { TextBox } from '@/components/foundation/TextBox';
@@ -24,33 +26,44 @@ export const ContactInformationSection = ({
       <>
         <Header text="Contact Information" type="title-2" />
         <Spacer size={12} />
-        <TextBox text="We'll send updates to the phone number and email below." />
+        <TextBox text="We'll send alerts to the phone number and email below." />
         <Spacer size={24} />
-        <UpdateCommunication
-          key={'phone'}
-          onClick={() =>
-            showAppModal({
-              content: <UpdateCommunicationText initNumber="(123) 456-0000" />,
-            })
-          }
-          label={<TextBox className="font-bold" text="Phone Number" />}
-          subLabel={phone}
-          methodName="Update"
-          divider={true}
-        />
-        <Spacer size={24} />
-        <UpdateCommunication
-          key={'email'}
-          onClick={() =>
-            showAppModal({
-              content: <UpdateCommunicationEmail email={email} />,
-            })
-          }
-          label={<TextBox className="font-bold" text="Email Address" />}
-          subLabel={email}
-          methodName="Update"
-          divider={false}
-        />
+        {phone && email ? (
+          <Column>
+            <UpdateCommunication
+              key={'phone'}
+              onClick={() =>
+                showAppModal({
+                  content: (
+                    <UpdateCommunicationText initNumber="(123) 456-0000" />
+                  ),
+                })
+              }
+              label={<TextBox className="font-bold" text="Phone Number" />}
+              subLabel={phone}
+              methodName="Update"
+              divider={true}
+            />
+            <Spacer size={24} />
+            <UpdateCommunication
+              key={'email'}
+              onClick={() =>
+                showAppModal({
+                  content: <UpdateCommunicationEmail email={email} />,
+                })
+              }
+              label={<TextBox className="font-bold" text="Email Address" />}
+              subLabel={email}
+              methodName="Update"
+              divider={false}
+            />
+          </Column>
+        ) : (
+          <ErrorInfoCard
+            className="mt-4"
+            errorText="We can't load your profile right now. Please try again later."
+          />
+        )}
       </>
     </Card>
   );
