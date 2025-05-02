aa2b7d8b347e1820b8f357210971b68cb76ceb15
Sundharam, Mythili Shanmuga <MythiliShanmuga_Sundharam@bcbst.com>
2025-05-02 12:50:28 +0000

    Merged PR 6949: Employer Provided benefit change
    Employer Provided benefit change
    
    Related work items: #63121

--- src/app/dashboard/components/MemberDashboard.tsx
+++ src/app/dashboard/components/MemberDashboard.tsx
@@ -341,30 +341,7 @@ const MemberDashboard = ({ data }: DashboardProps) => {
                 className="large-section"
                 employer="Ben Cole Co"
                 employerLogo={bcbstBlueLogo}
-                benefits={[
-                  {
-                    id: '45',
-                    providedBy: 'Davis Vision',
-                    contact: '1-800-456-9876',
-                    url: 'https://davis-vision.com',
-                  },
-                  {
-                    id: '87',
-                    providedBy: 'Nirmal Dental',
-                    contact: '1-800-367-9676',
-                    url: 'https://nirmaldental.com',
-                  },
-                  {
-                    id: '25',
-                    providedBy: 'Low Pharm',
-                    contact: '1-800-834-2465',
-                  },
-                  {
-                    id: '289',
-                    providedBy: 'Quant Labs',
-                    contact: '1-800-834-3465',
-                  },
-                ]}
+                benefits={data.employerProvidedBenefits}
               />
             )}
         </section>
