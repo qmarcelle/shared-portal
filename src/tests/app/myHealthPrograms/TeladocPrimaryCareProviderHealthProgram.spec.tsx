c4b3fcd94f6628cb7d17ed95fa5ddf4c65877939
Kamidi, Suryakumari <Suryakumari_Kamidi@bcbst.com>
2025-05-06 12:57:17 +0000

    Merged PR 7012: US - Teladoc SSO Integration 18780, 18781, 18782, 18783, 18784, 18785, 18778
    US : 18780 Teladoc Blood Pressure Management and BloodPressureManagement_VisibilityRule
    US : 18781 Teladoc Diabetes Management service and DiabetesManagement_VisibilityRule
    US : 18782 Teladoc Diabetes Prevention service and DiabetesPrevention_VisibilityRule
    US : 18783 Teladoc Primary Care Provider service and Teladoc_Primary360_VisibilityRule
    US : 18784 Teladoc Health General & Urgent Care and Teladoc_VisibilityRule
    US : 18785 Teladoc Mental Health service and MentalHealthSupport_VisibilityRule
    US : 18778 Teladoc Second Opinion service and ConsumerMedical_VisibilityRule
    
    For all the pages useTeladocHealth Button is redirected to **Teladoc SSO** and covered all the PZN rules and implemented unit testing accordingly
    
    Related work items: #18778, #18780, #18781, #18782, #18783, #18784, #18785

--- src/tests/app/myHealthPrograms/TeladocPrimaryCareProviderHealthProgram.spec.tsx
+++ src/tests/app/myHealthPrograms/TeladocPrimaryCareProviderHealthProgram.spec.tsx
@@ -1,7 +1,7 @@
 import { HealthProgramsResources } from '@/app/myHealth/healthProgramsResources/myHealthPrograms/components/HealthProgramsResources';
 import { HealthProgramType } from '@/app/myHealth/healthProgramsResources/myHealthPrograms/models/health_program_type';
 import { healthProgramsandResourcesDetails } from '@/app/myHealth/healthProgramsResources/myHealthPrograms/models/health_programs_resources';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 const renderUI = () => {
   return render(
@@ -9,9 +9,40 @@ const renderUI = () => {
       healthProgramDetails={healthProgramsandResourcesDetails.get(
         HealthProgramType.TeladocPrimaryCareProvider,
       )}
+      sessionData={null}
     />,
   );
 };
+const vRules = {
+  user: {
+    currUsr: {
+      plan: {
+        memCk: '123456',
+      },
+    },
+    vRules: {
+      primary360Eligible: false,
+      terminated: false,
+      wellnessOnly: false,
+      fsaOnly: false,
+      vision: true,
+      futureEffective: false,
+      katieBeckNoBenefitsElig: false,
+    },
+  },
+};
+jest.mock('src/auth', () => ({
+  auth: jest.fn(),
+}));
+
+// @ts-ignore
+delete window.location;
+// @ts-ignore
+window.location = new URL('https://localhost');
+const setHref = jest
+  .spyOn(window.location, 'href', 'set')
+  .mockImplementation(() => {});
+process.env.NEXT_PUBLIC_IDP_TELADOC = 'teladoc.com';
 
 describe('TeladocPrimaryCareProviderHealthProgram', () => {
   it('should render the UI correctly', async () => {
@@ -46,6 +77,22 @@ describe('TeladocPrimaryCareProviderHealthProgram', () => {
     screen.getByText(
       'Support with long-term conditions like diabetes, hypertension and mental health',
     );
+    expect(component).toMatchSnapshot();
+  });
+  it('should redirect to SSO link on click of Use Teladoc Health button', async () => {
+    const component = renderUI();
+    const mockAuth = jest.requireMock('src/auth').auth;
+    vRules.user.vRules.primary360Eligible = true;
+    mockAuth.mockResolvedValueOnce(vRules);
+
+    const button = screen.getByRole('button', {
+      name: 'Use Teladoc Health',
+    });
+
+    fireEvent.click(button);
+
+    expect(setHref).toHaveBeenCalledWith('/sso/launch?PartnerSpId=teladoc.com');
+
     expect(component).toMatchSnapshot();
   });
 });
