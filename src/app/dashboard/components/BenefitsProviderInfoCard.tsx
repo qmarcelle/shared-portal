8fe449c1e9a368d904322721d20afbe232448be4
Sundharam, Mythili Shanmuga <MythiliShanmuga_Sundharam@bcbst.com>
2025-05-08 11:46:41 +0000

    Merged PR 7105: Phone number and link fix
    Phone number and link fix
    
    Related work items: #63121

--- src/app/dashboard/components/BenefitsProviderInfoCard.tsx
+++ src/app/dashboard/components/BenefitsProviderInfoCard.tsx
@@ -19,7 +19,7 @@ export const BenefitsProviderInfoCard = ({
 }: BenefitsProviderInfoCardProps) => {
   return (
     <Card className={`${className}`}>
-      <a className="p-4 block" href={url}>
+      <a className="p-4 block" href={url} target="_blank">
         {url != null ? (
           <Title
             className="body-bold primary-color"
