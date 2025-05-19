91ccc3b4cc46d1b0c2e88fc3dd35884c6fdb85ab
CS, Navya <Navya_CS@bcbst.com>
2025-05-19 17:32:13 +0000

    Merged PR 7304: US 62258 Profile Information Error Handling
    US 62258 Profile Information Error Handling Changes
    
    Related work items: #62258

--- src/app/profileSettings/components/ProfileInformationCard.tsx
+++ src/app/profileSettings/components/ProfileInformationCard.tsx
@@ -1,3 +1,4 @@
+import { ErrorInfoCard } from '@/components/composite/ErrorInfoCard';
 import { UpdateRowWithStatus } from '@/components/composite/UpdateRowWithStatus';
 import { useAppModalStore } from '@/components/foundation/AppModal';
 import { Card } from '@/components/foundation/Card';
@@ -10,6 +11,7 @@ import { IComponent } from '@/components/IComponent';
 import { isNCQAEligible } from '@/visibilityEngine/computeVisibilityRules';
 import { VisibilityRules } from '@/visibilityEngine/rules';
 import { EditEmailProfileSettings } from '../journeys/EditEmailProfileSettings';
+import { UpdatePhoneNumberJourney } from '../journeys/UpdatePhoneNumberJourney';
 
 /* eslint-disable */
 
@@ -19,6 +21,9 @@ interface ProfileInformationCardProps extends IComponent {
   phoneNumber: string;
   email: string;
   visibilityRules?: VisibilityRules;
+  status: number;
+  emailVerified: boolean;
+  phoneVerified: boolean;
 }
 
 export const ProfileInformationCard = ({
@@ -27,6 +32,9 @@ export const ProfileInformationCard = ({
   email,
   phoneNumber,
   visibilityRules,
+  status,
+  emailVerified,
+  phoneVerified,
 }: ProfileInformationCardProps) => {
   const { showAppModal } = useAppModalStore();
   return (
@@ -51,7 +59,7 @@ export const ProfileInformationCard = ({
             <TextBox className="body-1" text={'DOB: ' + DOB} />
           </Column>
         </Card>
-        <Spacer size={32} />
+        <Spacer size={16} />
         {/* <UpdateRowWithStatus
           key={'Phone Number'}
           onClick={() =>
@@ -66,19 +74,49 @@ export const ProfileInformationCard = ({
           onOffLabelEnabled={false}
         /> */}
         <Spacer size={16} />
-        <UpdateRowWithStatus
-          key={'Email'}
-          onClick={() =>
-            showAppModal({
-              content: <EditEmailProfileSettings email={email} />,
-            })
-          }
-          label={<TextBox className="font-bold" text="Email Address" />}
-          subLabel={email}
-          methodName="Update"
-          divider={true}
-          onOffLabelEnabled={false}
-        />
+        {status === 200 ? (
+          <Column>
+            <Spacer size={16} />
+            <UpdateRowWithStatus
+              key={'Phone Number'}
+              onClick={() =>
+                showAppModal({
+                  content: (
+                    <UpdatePhoneNumberJourney phoneNumber={phoneNumber} />
+                  ),
+                })
+              }
+              label={<TextBox className="font-bold" text="Phone Number" />}
+              subLabel={phoneNumber}
+              methodName="Update"
+              divider={true}
+              onOffLabelEnabled={false}
+              profile="Phone Number"
+              emailVerified={emailVerified}
+              phoneVerified={phoneVerified}
+            />
+            <Spacer size={16} />
+            <UpdateRowWithStatus
+              key={'Email'}
+              onClick={() =>
+                showAppModal({
+                  content: <EditEmailProfileSettings email={email} />,
+                })
+              }
+              label={<TextBox className="font-bold" text="Email Address" />}
+              subLabel={email}
+              methodName="Update"
+              divider={true}
+              onOffLabelEnabled={false}
+              profile="Email Address"
+            />
+          </Column>
+        ) : (
+          <ErrorInfoCard
+            className="mt-4"
+            errorText="We can't load your profile right now. Please try again later."
+          />
+        )}
         {isNCQAEligible(visibilityRules) && (
           <>
             <Spacer size={16} />
