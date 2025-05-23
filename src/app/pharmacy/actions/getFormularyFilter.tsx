1b50292c2fd7949964d9f267aa52efcf511f09ab
Namala, Rohit <Rohit_Namala@bcbst.com>
2025-05-23 13:08:12 +0000

    Merged PR 8381: 67314 Formulary Fix
    67314 Formulary Fix
    
    Related work items: #67314

--- src/app/pharmacy/actions/getFormularyFilter.tsx
+++ src/app/pharmacy/actions/getFormularyFilter.tsx
@@ -14,36 +14,40 @@ export async function getFormularyFilter(
     const isBlueAdvantageExtraFormularyEnabled = (memberInfo: any) => {
       if (memberInfo == null) return false;
       return (
-        MEDICARE_MAPD_BLUEADVANTAGE_GROUP === memberInfo.groupID &&
+        MEDICARE_MAPD_BLUEADVANTAGE_GROUP === memberInfo.groupId &&
         MEDICARE_MAPD_BLUEADVANTAGE_GROUP_ABADTX00_PLAN_ID ===
           memberInfo.mPDPD_ID
       );
     };
-
+    logger.info('Formulary group ID: ', memberInfo.groupId);
     const sortByGroup =
       '87898,115617,129970,129969,130300,120970,115602,128361,119381,118069,122059,124377,115613,118068,121489,132570,116884,129884,129971,145130,142512,142513';
     if (
       sortByGroup != null &&
-      sortByGroup.split(',').includes(memberInfo.groupID)
+      sortByGroup.split(',').includes(memberInfo.groupId)
     ) {
-      if (memberInfo.groupID === '87898') {
+      if (memberInfo.groupId === '87898') {
         return 'Preferred';
       } else if (isBlueAdvantageExtraFormularyEnabled(memberInfo)) {
         return (
-          memberInfo.groupID +
+          memberInfo.groupId +
           '-' +
           MEDICARE_MAPD_BLUEADVANTAGE_GROUP_ABADTX00_PLAN_ID
         );
       } else {
-        return memberInfo.groupID;
+        return memberInfo.groupId;
       }
     } else if (visibilityRules?.rxEssentialEligible) {
+      logger.info('Formulary rxEssentialEligible');
       return 'Essential';
     } else if (visibilityRules?.rxEssentialPlusEligible) {
+      logger.info('Formulary rxEssentialPlusEligible');
       return 'EssentialPlus';
     } else if (visibilityRules?.rxPreferredEligible) {
+      logger.info('Formulary rxPreferredEligible');
       return 'Preferred';
     } else if (visibilityRules?.rxChoiceEligible) {
+      logger.info('Formulary rxChoiceEligible');
       return 'Choice';
     } else return 'Default';
   } catch (error) {
