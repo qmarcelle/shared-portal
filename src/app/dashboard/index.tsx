533451f72b31b0a930060292353cd9a3c2e1a815
Sundharam, Mythili Shanmuga <MythiliShanmuga_Sundharam@bcbst.com>
2025-05-27 13:54:54 +0000

    Merged PR 8481: Termed policy fix
    Termed policy fix
    
    Related work items: #71596

--- src/app/dashboard/index.tsx
+++ src/app/dashboard/index.tsx
@@ -65,10 +65,10 @@ const Dashboard = ({ data }: DashboardProps) => {
                 <Spacer size={8} />
               </>
             )}
-            {data.memberDetails?.coverageType?.length && (
+            {data.memberDetails.selectedPlan && (
               <>
                 <TextBox
-                  text={`Policies: ${data.memberDetails?.coverageType?.join(', ')}`}
+                  text={`Policies: ${data.memberDetails?.selectedPlan.policies}`}
                 />
                 <Spacer size={16} />
               </>
