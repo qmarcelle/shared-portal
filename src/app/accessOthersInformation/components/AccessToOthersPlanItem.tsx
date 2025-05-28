dcecbe4f8ff3961aad7f885efb173e38fda7b8ad
Nasar Theen, Thariq Sharif <Thariq_Sharif_Nasar_Theen@bcbst.com>
2025-05-28 12:41:04 +0000

    Merged PR 8468: US2178_GATaggings
    US2178_GATaggings
    
    Related work items: #2178, #72363

--- src/app/accessOthersInformation/components/AccessToOthersPlanItem.tsx
+++ src/app/accessOthersInformation/components/AccessToOthersPlanItem.tsx
@@ -13,6 +13,8 @@ import { Key } from 'react';
 import editIcon from '../../../../public/assets/edit.svg';
 import { OtherPlanDetails } from '../models/AccessOtherPlanDetails';
 
+import { AnalyticsData } from '@/models/app/analyticsData';
+import { googleAnalytics } from '@/utils/analytics';
 import { HideOthersPlan } from '../journeys/HideOthersPlan';
 interface AccessToOthersPlanItemProps extends IComponent {
   planDetails: OtherPlanDetails[] | null;
@@ -29,6 +31,18 @@ export const AccessToOthersPlanItem = ({
   className,
   icon = <Image src={editIcon} alt="" />,
 }: AccessToOthersPlanItemProps) => {
+  function trackOthersPlanItemUpdateAnalytics() {
+    const analytics: AnalyticsData = {
+      event: 'select_content',
+      click_text: 'Hide',
+      click_url: undefined,
+      page_section: undefined,
+      selection_type: 'modal',
+      element_category: "Others' Plan",
+      action: 'click',
+    };
+    googleAnalytics(analytics);
+  }
   const { showAppModal } = useAppModalStore();
   return (
     <Card
@@ -66,11 +80,12 @@ export const AccessToOthersPlanItem = ({
               className="font-bold primary-color "
               text="Hide"
               suffix={icon}
-              callback={() =>
+              callback={() => {
+                trackOthersPlanItemUpdateAnalytics();
                 showAppModal({
                   content: <HideOthersPlan />,
-                })
-              }
+                });
+              }}
             />
           </Column>
         ))}
