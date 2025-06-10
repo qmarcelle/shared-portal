3df9093381b531dc62b4e5b80a30018cca5ca242
Innani, Shruthi <Shruthi_Innani@bcbst.com>
2025-06-10 15:29:18 +0000

    Merged PR 9294: BUG-74509
    1. Updated the getPBE api call with dynamic key and value pair.
    2. In case of PR - sharing & permissions page we make call to PBE with umpi to fetch if the member has an account.
    3. PayPremium api call skip if the memeck is not available (non member dashboard)
    
    Related work items: #74509

--- src/actions/premiumPayInfo.ts
+++ src/actions/premiumPayInfo.ts
@@ -13,11 +13,20 @@ export async function getPremiumPayInfo(
   memberCk: string,
 ): Promise<PremiumPayResponse> {
   try {
-    const resp = await memberService.get<PremiumPayResponse>(
-      `/api/member/v1/members/byMemberCk/${memberCk}/premiumPayInfo`,
-    );
-    logger.info('payPremiumApiResponse', resp);
-    return resp.data;
+    if (memberCk) {
+      const resp = await memberService.get<PremiumPayResponse>(
+        `/api/member/v1/members/byMemberCk/${memberCk}/premiumPayInfo`,
+      );
+      logger.info('payPremiumApiResponse', resp);
+      return resp.data;
+    } else {
+      logger.info('PremiumPayInfo API - Memeck Not available');
+      return {
+        paymentDue: '',
+        currentBalance: '',
+        currentStmtBalance: '',
+      };
+    }
   } catch (error) {
     logger.error('PremiumPayInfo API Failed', error);
     return {
