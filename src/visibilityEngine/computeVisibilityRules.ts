0cb7cda5dabaa11ad1e8a42beb72f71035acf591
Oza, Utsavi <Utsavi_Oza@bcbst.com>
2025-06-11 06:56:11 +0000

    Merged PR 9314: Bug -75114 MSK Component Visibility Issue
    Updated visiblity rule for undefined return
    
    Related work items: #75114

--- src/visibilityEngine/computeVisibilityRules.ts
+++ src/visibilityEngine/computeVisibilityRules.ts
@@ -604,7 +604,7 @@ export function isMemberWellnessCenterEligible(
 }
 
 export function isMskEligible(rules: VisibilityRules | undefined) {
-  return rules?.isMskEligible;
+  return !!rules?.isMskEligible;
 }
 
 export const isHealthyMaternity = (rules: VisibilityRules | undefined) =>
